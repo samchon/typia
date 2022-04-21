0cdf3ffffe2eb2bd3a8404587ef02543ea14192c
Jeongho Nam <samchon.github@gmail.com>
2022-04-22 04:10:18 +0900

    Close #20 and Close #21

---
+++ src/factories/ExpressionFactory.ts
@@ -0,0 +1,49 @@
+import ts from "typescript";
+
+export namespace ExpressionFactory
+{
+    export function generate(input: any)
+    {
+        if (input instanceof Array)
+            return generate_array(input);
+        else if (typeof input === "object")
+            return generate_object(input);
+        else if (typeof input === "boolean")
+            return generate_boolean(input);
+        else if (typeof input === "string")
+            return generate_string(input);
+        else
+            throw new Error("Unknown type.");
+    }
+    
+    function generate_object(obj: object): ts.ObjectLiteralExpression
+    {
+        const properties = Object
+            .entries(obj)
+            .map(([key, value]) => ts.factory.createPropertyAssignment
+            (
+                key,
+                generate(value)
+            ));
+        return ts.factory.createObjectLiteralExpression(properties, true);
+    }
+
+    function generate_array(array: any[]): ts.ArrayLiteralExpression
+    {
+        return ts.factory.createArrayLiteralExpression
+        (
+            array.map(generate),
+            true
+        );
+    }
+
+    function generate_boolean(value: boolean): ts.Identifier
+    {
+        return ts.factory.createIdentifier(value.toString());
+    }
+
+    function generate_string(value: string): ts.StringLiteral
+    {
+        return ts.factory.createStringLiteral(value);
+    }
+}
---
+++ src/structures/TypeEntry.ts
@@ -0,0 +1,26 @@
+import ts from "typescript";
+import { hash } from "tstl/functional/hash";
+
+export class TypeEntry
+{
+    public constructor
+        (
+            public readonly type: ts.Type,
+            public readonly nullable: boolean,
+            public readonly required: boolean,
+        )
+    {
+    }
+
+    public equals(obj: TypeEntry): boolean
+    {
+        return this.type === obj.type
+            && this.nullable === obj.nullable
+            && this.required === obj.required;
+    }
+
+    public hashCode(): number
+    {
+        return hash(this.type, this.nullable, this.required);
+    }
+}
