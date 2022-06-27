ab5313147e3d5b324ba497b5237b8739400aae4e
Jeongho Nam <samchon@archisketch.cmo>
2022-06-27 16:02:32 +0900

    Try #117

---
+++ test/issues/117.ts
@@ -0,0 +1,11 @@
+import TSON from "../../src";
+
+function test<T extends object>(
+    array: T,
+    stringify: (value: T) => string,
+): void {
+    console.log(array.constructor.name, stringify(array));
+}
+const values: number[] = [1, 2, 3, 4];
+test(Buffer.from(values), (input) => TSON.stringify(input));
+test(new Uint8Array(values), (input) => TSON.stringify(input));
