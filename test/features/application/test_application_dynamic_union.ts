c3d2be338e001491ff7d9d80849223585cdfa235
Jeongho Nam <samchon.github@gmail.com>
2022-10-03 02:42:30 +0900

    Developing #153 and Close #217 - support JSON schema

---
+++ test/features/application/test_application_dynamic_union.ts
@@ -0,0 +1,45 @@
+import TSON from "../../../src";
+import { DynamicUnion } from "../../structures/DynamicUnion";
+import { _test_application } from "./_test_application";
+
+export const test_application_dynamic_union = _test_application(
+    "dynamic union",
+    TSON.application<[DynamicUnion]>(),
+    {
+        schemas: [
+            {
+                $ref: "#/components/schemas/DynamicUnion",
+            },
+        ],
+        components: {
+            schemas: {
+                DynamicUnion: {
+                    type: "object",
+                    properties: {},
+                    patternProperties: {
+                        "\\d+(\\.\\d+)?": {
+                            type: "string",
+                            nullable: false,
+                        },
+                        "(prefix_(.*))": {
+                            type: "string",
+                            nullable: false,
+                        },
+                        "((.*)_postfix)": {
+                            type: "string",
+                            nullable: false,
+                        },
+                        "(value_between_\\d+(\\.\\d+)?_and_\\d+(\\.\\d+)?)": {
+                            type: "number",
+                            nullable: false,
+                        },
+                    },
+                    nullable: false,
+                    jsDocTags: [],
+                },
+            },
+        },
+        purpose: "swagger",
+        prefix: "#/components/schemas",
+    },
+);
--- test/manual.js
+++ test/manual.js
@@ -7,6 +7,6 @@ runner.register({
     compiler: "ttypescript",
 });
 
-const program = require("./features/application/test_application_tag_format.ts");
+const program = require("./features/application/test_application_dynamic_composite.ts");
 for (const value of Object.values(program))
     if (typeof value === "function") value();
