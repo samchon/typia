c97e3e4b738ec9d4955670b9a690bb8085260334
Jeongho Nam <samchon.github@gmail.com>
2022-10-04 04:23:28 +0900

    Enhance template to pattern converter

--- test/features/application/test_application_template_union.ts
+++ test/features/application/test_application_template_union.ts
@@ -19,18 +19,20 @@ export const test_application_template_union = _test_application(
                         prefix: {
                             type: "string",
                             nullable: false,
-                            pattern: "(prefix_(.*))|(prefix_-?\\d+\\.?\\d*)",
+                            pattern:
+                                "^((prefix_(.*))|(prefix_-?\\d+\\.?\\d*))$",
                         },
                         postfix: {
                             type: "string",
                             nullable: false,
-                            pattern: "((.*)_postfix)|(-?\\d+\\.?\\d*_postfix)",
+                            pattern:
+                                "(((.*)_postfix)|(-?\\d+\\.?\\d*_postfix))$",
                         },
                         middle: {
                             type: "string",
                             nullable: false,
                             pattern:
-                                "(the_-?\\d+\\.?\\d*_value)|(the_false_value)|(the_true_value)",
+                                "^(the_false_value|the_true_value|(the_-?\\d+\\.?\\d*_value))$",
                         },
                         mixed: {
                             oneOf: [
@@ -38,7 +40,7 @@ export const test_application_template_union = _test_application(
                                     type: "string",
                                     nullable: false,
                                     pattern:
-                                        "(the_-?\\d+\\.?\\d*_value)|(the_A_value)|(the_B_value)",
+                                        "^(the_A_value|the_B_value|-?\\d+\\.?\\d*|true|false|(the_-?\\d+\\.?\\d*_value))$",
                                 },
                                 {
                                     type: "number",
