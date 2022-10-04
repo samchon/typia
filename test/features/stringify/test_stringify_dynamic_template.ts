9b7d6b51a46e055837d5f7a741b4e33a2a494b89
Jeongho Nam <samchon.github@gmail.com>
2022-10-05 03:31:52 +0900

    Close #222 - `TSON.stringify()` supports dynamic properties

---
+++ test/features/stringify/test_stringify_dynamic_template.ts
@@ -0,0 +1,9 @@
+import TSON from "../../../src";
+import { DynamicTemplate } from "../../structures/DynamicTemplate";
+import { _test_stringify } from "./_test_stringify";
+
+export const test_stringify_dynamic_template = _test_stringify(
+    "dynamic template",
+    DynamicTemplate.generate(),
+    (input) => TSON.stringify(input),
+);
