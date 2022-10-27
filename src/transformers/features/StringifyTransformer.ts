f1f33e205c1ebc00d41f1e1824f22a649eb506c1
jongwooo <han980817@gmail.com>
2022-10-28 02:24:12 +0900

    fix: Correct typo 'strigify' to 'stringify'

--- src/transformers/features/StringifyTransformer.ts
+++ src/transformers/features/StringifyTransformer.ts
@@ -31,6 +31,6 @@ export namespace StringifyTransformer {
 }
 
 const enum ErrorMessages {
-    NO_INPUT_VALUE = "Error on TSON.strigify(): no input value.",
+    NO_INPUT_VALUE = "Error on TSON.stringify(): no input value.",
     GENERIC_ARGUMENT = "Error on TSON.stringify(): non-specified generic argument.",
 }
