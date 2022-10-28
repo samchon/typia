b88be4dc18ade533a4d9f8d446b6f6a62466f810
Chris Ohk <utilforever@gmail.com>
2022-10-28 13:19:05 +0900

    fix: Correct typo 'arrray' to 'array'

--- test/features/create_validate/test_create_validate_array_union.ts
+++ test/features/create_validate/test_create_validate_array_union.ts
@@ -3,7 +3,7 @@ import { ArrayUnion } from "../../structures/ArrayUnion";
 import { _test_validate } from "./../validate/_test_validate";
 
 export const test_create_validate_array_union = _test_validate(
-    "union arrray",
+    "union array",
     ArrayUnion.generate,
     TSON.createValidate<ArrayUnion>(),
     ArrayUnion.SPOILERS,
