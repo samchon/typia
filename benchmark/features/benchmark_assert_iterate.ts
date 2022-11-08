d85ed5ca14cd1be5f65bed3de69ad2f1c487d881
Jeongho Nam <samchon.github@gmail.com>
2022-11-09 05:28:50 +0900

    Benchmark again with release mode

--- benchmark/features/benchmark_assert_iterate.ts
+++ benchmark/features/benchmark_assert_iterate.ts
@@ -1,5 +1,5 @@
-import { TSchema, Type } from "@sinclair/typebox";
-import { TypeCompiler } from "@sinclair/typebox/compiler";
+import { TSchema } from "@sinclair/typebox";
+import { TypeCheck } from "@sinclair/typebox/compiler";
 import * as tr from "class-transformer";
 import { ClassConstructor } from "class-transformer";
 import * as cv from "class-validator";
@@ -13,17 +13,19 @@ import { ArrayRecursive } from "../../test/structures/ArrayRecursive";
 import { ArrayRecursiveUnionExplicit } from "../../test/structures/ArrayRecursiveUnionExplicit";
 import { ArrayRecursiveUnionImplicit } from "../../test/structures/ArrayRecursiveUnionImplicit";
 import { ObjectHierarchical } from "../../test/structures/ObjectHierarchical";
+import { ObjectRecursive } from "../../test/structures/ObjectRecursive";
 import { ObjectSimple } from "../../test/structures/ObjectSimple";
 import { ObjectUnionExplicit } from "../../test/structures/ObjectUnionExplicit";
 import { ObjectUnionImplicit } from "../../test/structures/ObjectUnionImplicit";
 import { UltimateUnion } from "../../test/structures/UltimateUnion";
 // BENCHMARK PROGRAM
-import { AssertThrowBenchmarker } from "../internal/AssertThrowBenchmarker";
+import { AssertIterateBenchmarker } from "../internal/AssertIterateBenchmarker";
 // CLASS-VALIDATOR
 import { CvArrayRecursive } from "../structures/class-validator/CvArrayRecursive";
 import { CvArrayRecursiveUnionExplicit } from "../structures/class-validator/CvArrayRecursiveUnionExplicit";
 import { CvArrayRecursiveUnionImplicit } from "../structures/class-validator/CvArrayRecursiveUnionImplicit";
 import { CvObjectHierarchical } from "../structures/class-validator/CvObjectHierarchical";
+import { CvObjectRecursive } from "../structures/class-validator/CvObjectRecursive";
 import { CvObjectSimple } from "../structures/class-validator/CvObjectSimple";
 import { CvObjectUnionExplicit } from "../structures/class-validator/CvObjectUnionExplicit";
 import { CvObjectUnionImplicit } from "../structures/class-validator/CvObjectUnionImplicit";
@@ -32,25 +34,28 @@ import { IoTsArrayRecursive } from "../structures/io-ts/IoTsArrayRecursive";
 import { IoTsArrayRecursiveUnionExplicit } from "../structures/io-ts/IoTsArrayRecursiveUnionExplicit";
 import { IoTsArrayRecursiveUnionImplicit } from "../structures/io-ts/IoTsArrayRecursiveUnionImplicit";
 import { IoTsObjectHierarchical } from "../structures/io-ts/IoTsObjectHierarchical";
+import { IoTsObjectRecursive } from "../structures/io-ts/IoTsObjectRecursive";
 import { IoTsObjectSimple } from "../structures/io-ts/IoTsObjectSimple";
 import { IoTsObjectUnionExplicit } from "../structures/io-ts/IoTsObjectUnionExplicit";
 import { IoTsObjectUnionImplicit } from "../structures/io-ts/IoTsObjectUnionImplicit";
 import { IoTsUltimateUnion } from "../structures/io-ts/IoTsUltimateUnion";
 import { IoTsUtils } from "../structures/io-ts/IoTsUtils";
 // TYPEBOX TYPES
-import { __TypeBoxArrayRecursive } from "../structures/typebox/TypeBoxArrayRecursive";
-import { __TypeBoxArrayRecursiveUnionExplicit } from "../structures/typebox/TypeBoxArrayRecursiveUnionExplicit";
-import { __TypeBoxArrayRecursiveUnionImplicit } from "../structures/typebox/TypeBoxArrayRecursiveUnionImplicit";
-import { __TypeBoxObjectHierarchical } from "../structures/typebox/TypeBoxObjectHierarchical";
-import { __TypeBoxObjectSimple } from "../structures/typebox/TypeBoxObjectSimple";
-import { __TypeBoxObjectUnionExplicit } from "../structures/typebox/TypeBoxObjectUnionExplicit";
-import { __TypeBoxObjectUnionImplicit } from "../structures/typebox/TypeBoxObjectUnionImplicit";
-import { __TypeBoxUltimateUnion } from "../structures/typebox/TypeBoxUltimateUnion";
+import { TypeBoxArrayRecursive } from "../structures/typebox/TypeBoxArrayRecursive";
+import { TypeBoxArrayRecursiveUnionExplicit } from "../structures/typebox/TypeBoxArrayRecursiveUnionExplicit";
+import { TypeBoxArrayRecursiveUnionImplicit } from "../structures/typebox/TypeBoxArrayRecursiveUnionImplicit";
+import { TypeBoxObjectHierarchical } from "../structures/typebox/TypeBoxObjectHierarchical";
+import { TypeBoxObjectRecursive } from "../structures/typebox/TypeBoxObjectRecursive";
+import { TypeBoxObjectSimple } from "../structures/typebox/TypeBoxObjectSimple";
+import { TypeBoxObjectUnionExplicit } from "../structures/typebox/TypeBoxObjectUnionExplicit";
+import { TypeBoxObjectUnionImplicit } from "../structures/typebox/TypeBoxObjectUnionImplicit";
+import { TypeBoxUltimateUnion } from "../structures/typebox/TypeBoxUltimateUnion";
 // ZOD TYPES
 import { ZodArrayRecursive } from "../structures/zod/ZodArrayRecursive";
 import { ZodArrayRecursiveUnionExplicit } from "../structures/zod/ZodArrayRecursiveUnionExplicit";
 import { ZodArrayRecursiveUnionImplicit } from "../structures/zod/ZodArrayRecursiveUnionImplicit";
 import { ZodObjectHierarchical } from "../structures/zod/ZodObjectHierarchical";
+import { ZodObjectRecursive } from "../structures/zod/ZodObjectRecursive";
 import { ZodObjectSimple } from "../structures/zod/ZodObjectSimple";
 import { ZodObjectUnionExplicit } from "../structures/zod/ZodObjectUnionExplicit";
 import { ZodObjectUnionImplicit } from "../structures/zod/ZodObjectUnionImplicit";
@@ -69,44 +74,38 @@ class CustomError extends Error {
     }
 }
 
-const assertTypeBox = <S extends TSchema>(schema: S) => {
-    const program = TypeCompiler.Compile(Type.Array(schema));
-    return <T>(input: T[]) => {
-        if (program.Check(input)) return input;
-
-        const iterator = program.Errors(input);
-        throw new CustomError(iterator.next().value);
+const assertTypeBox =
+    <S extends TSchema>(program: TypeCheck<S>) =>
+    <T>(input: T) => {
+        const value = program.Errors(input).next().value;
+        if (value) throw new CustomError(value);
+        return input;
     };
-};
-const assertIoTs = <S extends t.Mixed>(type: S) => {
-    const program = t.array(type);
-    return <T>(input: T[]) => {
-        const validation = program.decode(input);
+const assertIoTs =
+    <S extends t.Mixed>(type: S) =>
+    <T>(input: T) => {
+        const validation = type.decode(input);
         const paths: string[] = IoTsUtils.getPaths(validation);
 
         if (paths.length) throw new CustomError(paths);
         return input;
     };
-};
-const assertZod = <S extends z.ZodTypeAny>(type: S) => {
-    const program = z.array(type);
-    return <T>(input: T[]) => {
-        const res = program.safeParse(input);
+const assertZod =
+    <S extends z.ZodTypeAny>(type: S) =>
+    <T>(input: T) => {
+        const res = type.safeParse(input);
         if (res.success) return input;
         throw new CustomError(res.error);
     };
-};
 const assertClassValidator =
     <S extends object>(schema: ClassConstructor<S>) =>
-    <T>(input: T[]) => {
-        const errors: cv.ValidationError[] = [];
-        for (const elem of input)
-            errors.push(...cv.validateSync(tr.plainToInstance(schema, elem)));
-        if (errors.length) throw new CustomError(errors);
+    <T>(input: T) => {
+        const result = cv.validateSync(tr.plainToInstance(schema, input));
+        if (result.length) throw new CustomError(result);
         return input;
     };
 
-const prepare = AssertThrowBenchmarker.prepare([
+const prepare = AssertIterateBenchmarker.prepare([
     "typescript-json",
     "typebox",
     "io-ts",
@@ -117,129 +116,118 @@ const prepare = AssertThrowBenchmarker.prepare([
 /* -----------------------------------------------------------
     DO BENCHMARK
 ----------------------------------------------------------- */
-const assertType_po_throw_pc = () => [
+const assert_po_iterate_pc = () => [
     prepare(
         "object (simple)",
         () => ObjectSimple.generate(),
-        () => ObjectSimple.trail(),
         {
-            "typescript-json": TSON.createAssert<ObjectSimple[]>(),
+            "typescript-json": TSON.createAssert<ObjectSimple>(),
+            typebox: assertTypeBox(TypeBoxObjectSimple),
             "io-ts": assertIoTs(IoTsObjectSimple),
-            "class-validator": assertClassValidator(CvObjectSimple),
             zod: assertZod(ZodObjectSimple),
-            typebox: assertTypeBox(__TypeBoxObjectSimple),
+            "class-validator": assertClassValidator(CvObjectSimple),
         },
         ObjectSimple.SPOILERS,
     ),
     prepare(
         "object (hierarchical)",
         () => ObjectHierarchical.generate(),
-        () => ObjectHierarchical.trail(),
         {
-            "typescript-json": TSON.createAssert<ObjectHierarchical[]>(),
+            "typescript-json": TSON.createAssert<ObjectHierarchical>(),
             "io-ts": assertIoTs(IoTsObjectHierarchical),
             "class-validator": assertClassValidator(CvObjectHierarchical),
             zod: assertZod(ZodObjectHierarchical),
-            typebox: assertTypeBox(__TypeBoxObjectHierarchical),
+            typebox: assertTypeBox(TypeBoxObjectHierarchical),
         },
         ObjectHierarchical.SPOILERS,
     ),
     prepare(
         "object (recursive)",
-        () => ObjectUnionImplicit.generate(),
-        () => ObjectUnionImplicit.trail(),
+        () => ObjectRecursive.generate(),
         {
-            "typescript-json": TSON.createAssert<ObjectUnionImplicit[]>(),
-            "io-ts": assertIoTs(IoTsObjectUnionImplicit),
-            "class-validator": assertClassValidator(CvObjectUnionImplicit),
-            zod: assertZod(ZodObjectUnionImplicit),
-            typebox: assertTypeBox(__TypeBoxObjectUnionImplicit),
+            "typescript-json": TSON.createAssert<ObjectRecursive>(),
+            "io-ts": assertIoTs(IoTsObjectRecursive),
+            "class-validator": assertClassValidator(CvObjectRecursive),
+            zod: assertZod(ZodObjectRecursive),
+            typebox: assertTypeBox(TypeBoxObjectRecursive),
         },
-        ObjectUnionImplicit.SPOILERS,
+        ObjectRecursive.SPOILERS,
     ),
     prepare(
         "object (union, explicit)",
         () => ObjectUnionExplicit.generate(),
-        () => ObjectUnionExplicit.trail(),
         {
-            "typescript-json": TSON.createAssert<ObjectUnionExplicit[]>(),
+            "typescript-json": TSON.createAssert<ObjectUnionExplicit>(),
             "io-ts": assertIoTs(IoTsObjectUnionExplicit),
             "class-validator": assertClassValidator(CvObjectUnionExplicit),
             zod: assertZod(ZodObjectUnionExplicit),
-            typebox: assertTypeBox(__TypeBoxObjectUnionExplicit),
+            typebox: assertTypeBox(TypeBoxObjectUnionExplicit),
         },
         ObjectUnionExplicit.SPOILERS,
     ),
     prepare(
         "object (union, implicit)",
         () => ObjectUnionImplicit.generate(),
-        () => ObjectUnionImplicit.trail(),
         {
-            "typescript-json": TSON.createAssert<ObjectUnionImplicit[]>(),
+            "typescript-json": TSON.createAssert<ObjectUnionImplicit>(),
             "io-ts": assertIoTs(IoTsObjectUnionImplicit),
             "class-validator": assertClassValidator(CvObjectUnionImplicit),
             zod: assertZod(ZodObjectUnionImplicit),
-            typebox: assertTypeBox(__TypeBoxObjectUnionImplicit),
+            typebox: assertTypeBox(TypeBoxObjectUnionImplicit),
         },
         ObjectUnionImplicit.SPOILERS,
     ),
     prepare(
         "array (recursive)",
         () => ArrayRecursive.generate(),
-        () => ArrayRecursive.trail(),
         {
-            "typescript-json": TSON.createAssert<ArrayRecursive[]>(),
+            "typescript-json": TSON.createAssert<ArrayRecursive>(),
             "io-ts": assertIoTs(IoTsArrayRecursive),
             "class-validator": assertClassValidator(CvArrayRecursive),
             zod: assertZod(ZodArrayRecursive),
-            typebox: assertTypeBox(__TypeBoxArrayRecursive),
+            typebox: assertTypeBox(TypeBoxArrayRecursive),
         },
         ArrayRecursive.SPOILERS,
     ),
     prepare(
         "array (union, explicit)",
         () => ArrayRecursiveUnionExplicit.generate(),
-        () => ArrayRecursiveUnionExplicit.trail(),
         {
-            "typescript-json":
-                TSON.createAssert<ArrayRecursiveUnionExplicit[]>(),
+            "typescript-json": TSON.createAssert<ArrayRecursiveUnionExplicit>(),
             "io-ts": assertIoTs(IoTsArrayRecursiveUnionExplicit),
             "class-validator": assertClassValidator(
                 CvArrayRecursiveUnionExplicit,
             ),
             zod: assertZod(ZodArrayRecursiveUnionExplicit),
-            typebox: assertTypeBox(__TypeBoxArrayRecursiveUnionExplicit),
+            typebox: assertTypeBox(TypeBoxArrayRecursiveUnionExplicit),
         },
         ArrayRecursiveUnionExplicit.SPOILERS,
     ),
     prepare(
         "array (union, implicit)",
         () => ArrayRecursiveUnionImplicit.generate(),
-        () => ArrayRecursiveUnionImplicit.trail(),
         {
-            "typescript-json":
-                TSON.createAssert<ArrayRecursiveUnionImplicit[]>(),
+            "typescript-json": TSON.createAssert<ArrayRecursiveUnionImplicit>(),
             "io-ts": assertIoTs(IoTsArrayRecursiveUnionImplicit),
             "class-validator": assertClassValidator(
                 CvArrayRecursiveUnionImplicit,
             ),
             zod: assertZod(ZodArrayRecursiveUnionImplicit),
-            typebox: assertTypeBox(__TypeBoxArrayRecursiveUnionImplicit),
+            typebox: assertTypeBox(TypeBoxArrayRecursiveUnionImplicit),
         },
         ArrayRecursiveUnionImplicit.SPOILERS,
     ),
     prepare(
         "ultimate union",
         () => UltimateUnion.generate(),
-        () => UltimateUnion.trail(),
         {
-            "typescript-json": TSON.createAssert<UltimateUnion[]>(),
+            "typescript-json": TSON.createAssert<UltimateUnion>(),
             "io-ts": assertIoTs(IoTsUltimateUnion),
             "class-validator": null,
             zod: assertZod(ZodUltimateUnion),
-            typebox: assertTypeBox(__TypeBoxUltimateUnion),
+            typebox: assertTypeBox(TypeBoxUltimateUnion),
         },
         UltimateUnion.SPOILERS,
     ),
 ];
-export { assertType_po_throw_pc as benchmark_assert_type_throw };
+export { assert_po_iterate_pc as benchmark_assert_iterate };
