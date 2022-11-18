d4f84f7de07175c0df7f9f4bb6a8d4290931774e
Jeongho Nam <samchon.github@gmail.com>
2022-11-19 03:15:12 +0900

    Complement #344 - `IValidatin.ISuccess<T>.data`

--- src/functional/$report.ts
+++ src/functional/$report.ts
@@ -1,7 +1,6 @@
 import { IValidation } from "../IValidation";
 
-export const $report = (output: IValidation) => {
-    const array: IValidation.IError[] = output.errors;
+export const $report = (array: IValidation.IError[]) => {
     const reportable = (path: string): boolean => {
         if (array.length === 0) return true;
         const last: string = array[array.length - 1]!.path;
