9c049a00a7f0af1806bb49bd9d61cb3bfe849b3f
lemon-mint <ice.lemons.mint@gmail.com>
2022-11-25 23:08:03 +0900

    Add $proto_i64_decode

--- src/functional/$proto_i64.ts
+++ src/functional/$proto_i64.ts
@@ -17,3 +17,21 @@ export function $proto_i64_encode(
 
     return offset;
 }
+
+export function $proto_i64_decode(
+    buf: Uint8Array,
+    offset: number,
+): [value: bigint, offset: number] {
+    const value =
+        (BigInt(buf[offset]!) << BigInt(0)) |
+        (BigInt(buf[offset + 1]!) << BigInt(8)) |
+        (BigInt(buf[offset + 2]!) << BigInt(16)) |
+        (BigInt(buf[offset + 3]!) << BigInt(24)) |
+        (BigInt(buf[offset + 4]!) << BigInt(32)) |
+        (BigInt(buf[offset + 5]!) << BigInt(40)) |
+        (BigInt(buf[offset + 6]!) << BigInt(48)) |
+        (BigInt(buf[offset + 7]!) << BigInt(56));
+    offset += 8;
+
+    return [BigInt.asIntN(64, value), offset];
+}
