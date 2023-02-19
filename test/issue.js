f8dd880a18cb6261635044ac6bd128e34d8c6a12
Jeongho Nam <samchon.github@gmail.com>
2023-02-19 21:29:32 +0900

    Close #239 - no way to trace exact recursive alias name

--- test/issue.js
+++ test/issue.js
@@ -2,8 +2,10 @@ const SuppressWarnings = require("suppress-warnings");
 const runner = require("ts-node");
 
 SuppressWarnings([() => true]);
+
+process.cwd(__dirname);
 runner.register({
-    project: __dirname + "/tsconfig.json",
+    project: __dirname + "/tsconfig.issues.json",
     compiler: "ttypescript",
 });
 require(`./issues/${process.argv[2]}.ts`);
---
+++ test/issues/239.ts
@@ -0,0 +1,26 @@
+import typia from "typia";
+
+type DeepPartial<T> = T extends object
+    ? {
+          [P in keyof T]?: DeepPartial<T[P]>;
+      }
+    : T;
+
+const invalidInput = { child: "wrong" };
+
+declare class TestChildClass {
+    a: string;
+}
+
+declare class TestClass {
+    child: TestChildClass;
+}
+
+const res4 = typia.validateEquals<TestClass>(invalidInput);
+console.log("correct expected:", res4);
+
+const res5 = typia.validateEquals<Partial<TestClass>>(invalidInput);
+console.log("correct expected:", res5);
+
+const res6 = typia.validateEquals<DeepPartial<TestClass>>(invalidInput);
+console.log("incorrect expected:", res6);
