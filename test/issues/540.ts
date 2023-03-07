6d1d593915be85b64170d4258d4a645323ee86a2
Jeongho Nam <samchon.github@gmail.com>
2023-03-08 01:38:21 +0900

    Complement #540 - more exact uuid validation regex

--- test/issues/540.ts
+++ test/issues/540.ts
@@ -1,4 +1,4 @@
-import { v1, v3, v4, v5 } from "uuid";
+import { v1, v3, v4, v5, validate } from "uuid";
 
 import { $is_uuid } from "../../src/functional/$is_uuid";
 
@@ -9,6 +9,15 @@ for (let i: number = 0; i < 100_000; ++i) {
         v4(),
         v5("https://www.w3.org/", v5.URL),
     ];
-    if (uuids.every((u) => $is_uuid(u) === false))
+    if (uuids.every((u) => $is_uuid(u) !== validate(u)))
         throw new Error("Failed to test the UUIDs");
 }
+
+const uuid: string = (() => {
+    const uuid: string = v4();
+    return uuid
+        .split("-")
+        .map((word) => "1" + word.substring(1))
+        .join("-");
+})();
+console.log(uuid, $is_uuid(uuid), validate(uuid));
