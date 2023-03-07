b4e6d841667ae394b24a1233dfbe6a8dcbfc902f
Jeongho Nam <samchon.github@gmail.com>
2023-03-08 01:38:21 +0900

    Complement #540 - more exact uuid validation regex

--- src/functional/$is_uuid.ts
+++ src/functional/$is_uuid.ts
@@ -1,4 +1,5 @@
 export function $is_uuid(str: string): boolean {
     return REGEX.test(str);
 }
-const REGEX = /^[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}$/i;
+const REGEX =
+    /^(?:[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}|00000000-0000-0000-0000-000000000000)$/i;
