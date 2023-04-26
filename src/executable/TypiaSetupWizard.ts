fa394e96fdb797f429938177097f763d337c5358
Jeongho Nam <samchon.github@gmail.com>
2023-04-26 22:03:37 +0900

    Support both TS 4.9 and TS 5.0
    @nonara, author of `ts-patch` said that `ts-patch@3` will support only `typescript@5` version.
    
    Therefore, made setup wizard to install `ts-patch` first, and determine whether to install TS 4 or TS 5, considering version of the `ts-patch`
    
      - `ts-patch@2`: `typescript@4.9.5`
      - `ts-patch@3`: `typescript@latest`

--- src/executable/TypiaSetupWizard.ts
+++ src/executable/TypiaSetupWizard.ts
@@ -20,16 +20,31 @@ export namespace TypiaSetupWizard {
         const pack: PackageManager = await PackageManager.mount();
         const args: IArguments = await ArgumentParser.parse(pack)(inquiry);
 
-        // INSTALL TYPESCRIPT
-        pack.install({ dev: true, modulo: "typescript", version: "latest" });
+        // INSTALL TYPESCRIPT COMPILERS
+        pack.install({ dev: true, modulo: "ts-patch", version: "latest" });
+        pack.install({ dev: true, modulo: "ts-node", version: "latest" });
+        pack.install({
+            dev: true,
+            modulo: "typescript",
+            version: (() => {
+                const version: string = (() => {
+                    try {
+                        return require("ts-patch/package.json")?.version ?? "";
+                    } catch {
+                        return "";
+                    }
+                })();
+                return Number(version.split(".")[0] ?? "") >= 3
+                    ? "latest"
+                    : "4.9.5";
+            })(),
+        });
         args.project ??= (() => {
             CommandExecutor.run("npx tsc --init");
             return (args.project = "tsconfig.json");
         })();
-        pack.install({ dev: true, modulo: "ts-node", version: "latest" });
 
-        // INSTALL COMPILER
-        pack.install({ dev: true, modulo: "ts-patch", version: "3.0.0-beta3" });
+        // SETUP TRANSFORMER
         await pack.save((data) => {
             data.scripts ??= {};
             if (
