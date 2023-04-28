42ab6f53fa051c3253fb796a330a9b8f991c1403
Jeongho Nam <samchon.github@gmail.com>
2023-04-28 13:29:38 +0900

    Add sitemap.xml

--- website/deploy.js
+++ website/deploy.js
@@ -1,16 +1,7 @@
 const cp = require('child_process');
 const deploy = require('gh-pages');
 
-const clear = () => [".next", "out"].forEach(
-    (directory) => cp.execSync(
-        `rimraf ${directory}`, 
-        { stdio: 'inherit' }
-    )
-);
-
-clear();
-cp.execSync('npx next build', { stdio: 'inherit' });
-cp.execSync('npx next export', { stdio: 'inherit' });
+cp.execSync('npm run build', { stdio: 'inherit' });
 
 deploy.publish(
     "out", 
