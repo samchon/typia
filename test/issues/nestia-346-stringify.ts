fdbce13e029ec1154d7b0e448f9971a47cae1f8e
Jeongho Nam <samchon.github@gmail.com>
2023-06-21 03:29:59 +0900

    Help #638 - shows how to utilize JSON schema

--- test/issues/nestia-346-stringify.ts
+++ test/issues/nestia-346-stringify.ts
@@ -1,20 +1,14 @@
 import fs from "fs";
 import typia from "typia";
 
-import { primitive_equal_to } from "../helpers/primitive_equal_to";
-import { ArrayRepeatedUnionWithTuple } from "../structures/ArrayRepeatedUnionWithTuple";
+type JsonObject = { [Key in string]?: JsonValue };
+interface JsonArray extends Array<JsonValue> {}
+type JsonValue = string | number | boolean | JsonObject | JsonArray | null;
 
-const factory = typia.createStringify<ArrayRepeatedUnionWithTuple>();
+const factory = typia.createAssertStringify<JsonValue>();
 
 fs.writeFileSync(
     __dirname + "/nestia-346-stringify.out.js",
     factory.toString(),
     "utf8",
 );
-
-const data = ArrayRepeatedUnionWithTuple.generate();
-const invert = JSON.parse(factory(data));
-const tracer = {};
-
-primitive_equal_to(data, invert, tracer);
-console.log(tracer);
