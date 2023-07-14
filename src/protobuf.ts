00f3dd6dd31d19a3499d7084ec7afa8f1787af52
Jeongho Nam <samchon.github@gmail.com>
2023-07-15 02:07:53 +0900

    Edit README content

--- src/protobuf.ts
+++ src/protobuf.ts
@@ -1,4 +1,17 @@
+import { IValidation } from "./IValidation";
+import { Primitive } from "./Primitive";
+
+/* ===========================================================
+    PROTOCOL BUFFER
+      - MESSAGE
+      - DECODE
+      - ENCODE
+      - FACTORY FUNCTIONS
+==============================================================
+    SCHEMA
+----------------------------------------------------------- */
 export function message(): never;
+
 export function message<T>(): string;
 
 /**
@@ -7,6 +20,96 @@ export function message<T>(): string;
 export function message(): never {
     halt("message");
 }
+
+/* -----------------------------------------------------------
+    DECODE
+----------------------------------------------------------- */
+export function decode(input: Uint8Array): never;
+
+export function decode<T>(input: Uint8Array): Primitive<T>;
+
+/**
+ * @internal
+ */
+export function decode(): never {
+    halt("decode");
+}
+
+export function isDecode(input: Uint8Array): never;
+
+export function isDecode<T>(input: Uint8Array): Primitive<T> | null;
+
+/**
+ * @internal
+ */
+export function isDecode(): never {
+    halt("isDecode");
+}
+
+export function assertDecode(input: Uint8Array): never;
+
+export function assertDecode<T>(input: Uint8Array): Primitive<T>;
+
+/**
+ * @internal
+ */
+export function assertDecode(): never {
+    halt("assertDecode");
+}
+
+export function validateDecode(input: Uint8Array): never;
+
+export function validateDecode<T>(input: Uint8Array): IValidation<Primitive<T>>;
+
+/**
+ * @internal
+ */
+export function validateDecode(): never {
+    halt("validateDecode");
+}
+
+/* -----------------------------------------------------------
+    ENCODE
+----------------------------------------------------------- */
+export function encode<T>(input: T): Uint16Array;
+
+/**
+ * @internal
+ */
+export function encode(): never {
+    halt("encode");
+}
+
+export function isEncode<T>(input: T): Uint16Array | null;
+
+/**
+ * @internal
+ */
+export function isEncode(): never {
+    halt("isEncode");
+}
+
+export function assertEncode<T>(input: T): Uint16Array;
+
+/**
+ * @internal
+ */
+export function assertEncode(): never {
+    halt("assertEncode");
+}
+
+export function validateEncode<T>(input: T): IValidation<Uint16Array>;
+
+/**
+ * @internal
+ */
+export function validateEncode(): never {
+    halt("validateEncode");
+}
+
+/* -----------------------------------------------------------
+    FACTORY FUNCTIONS
+----------------------------------------------------------- */
 
 /**
  * @internal
