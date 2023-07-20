35ec65f1834423ddc8bae2c8246ba0ebd810ddce
Jeongho Nam <samchon.github@gmail.com>
2023-07-21 03:13:53 +0900

    Prepare protobuf interfaces

--- src/protobuf.ts
+++ src/protobuf.ts
@@ -110,6 +110,95 @@ export function validateEncode(): never {
 /* -----------------------------------------------------------
     FACTORY FUNCTIONS
 ----------------------------------------------------------- */
+export function createDecode(): never;
+export function createDecode<T>(): (input: Uint8Array) => Primitive<T>;
+
+/**
+ * @internal
+ */
+export function createDecode<T>(): (input: Uint8Array) => Primitive<T> {
+    halt("createDecode");
+}
+
+export function createIsDecode(): never;
+export function createIsDecode<T>(): (input: Uint8Array) => Primitive<T> | null;
+
+/**
+ * @internal
+ */
+export function createIsDecode<T>(): (
+    input: Uint8Array,
+) => Primitive<T> | null {
+    halt("createIsDecode");
+}
+
+export function createAssertDecode(): never;
+export function createAssertDecode<T>(): (input: Uint8Array) => Primitive<T>;
+
+/**
+ * @internal
+ */
+export function createAssertDecode<T>(): (input: Uint8Array) => Primitive<T> {
+    halt("createAssertDecode");
+}
+
+export function createValidateDecode(): never;
+export function createValidateDecode<T>(): (
+    input: Uint8Array,
+) => IValidation<Primitive<T>>;
+
+/**
+ * @internal
+ */
+export function createValidateDecode<T>(): (
+    input: Uint8Array,
+) => IValidation<Primitive<T>> {
+    halt("createValidateDecode");
+}
+
+export function createEncode(): never;
+export function createEncode<T>(): (input: T) => Uint8Array;
+
+/**
+ * @internal
+ */
+export function createEncode<T>(): (input: T) => Uint8Array {
+    halt("createEncode");
+}
+
+export function createIsEncode(): never;
+export function createIsEncode<T>(): (input: T) => Uint8Array | null;
+
+/**
+ * @internal
+ */
+export function createIsEncode<T>(): (input: T) => Uint8Array | null {
+    halt("createIsEncode");
+}
+
+export function createAssertEncode(): never;
+export function createAssertEncode<T>(): (input: T) => Uint8Array;
+
+/**
+ * @internal
+ */
+export function createAssertEncode<T>(): (input: T) => Uint8Array {
+    halt("createAssertEncode");
+}
+
+export function createValidateEncode(): never;
+export function createValidateEncode<T>(): (
+    input: T,
+) => IValidation<Uint8Array>;
+
+/**
+ * @internal
+ */
+export function createValidateEncode<T>(): (
+    input: T,
+) => IValidation<Uint8Array> {
+    halt("createValidateEncode");
+}
 
 /**
  * @internal
