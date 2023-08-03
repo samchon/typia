74ae59b6a46a1c650f36fbafef06d49100a18d11
Jeongho Nam <samchon.github@gmail.com>
2023-08-03 17:38:44 +0900

    Complement #736 - missed pattern checking

--- test/issues/736.ts
+++ test/issues/736.ts
@@ -1,6 +1,8 @@
 import benchmark from "benchmark";
 import typia from "typia";
 
+import { $string } from "typia/lib/functional/$string";
+
 interface ISmallStrings {
     /**
      * @minLength 41
@@ -46,39 +48,7 @@ const features: Record<string, (str: string) => string> = {
             ? JSON.stringify(str)
             : `"${result}"`;
     },
-    advanced: (str: string): string => {
-        const length = str.length;
-        if (length > 41) return JSON.stringify(str);
-
-        let result = "";
-        let last = -1;
-        let point = 255;
-
-        // eslint-disable-next-line
-        for (let i = 0; i < length; ++i) {
-            point = str.charCodeAt(i);
-            if (point < 32) {
-                return JSON.stringify(str);
-            }
-            if (point >= 0xd800 && point <= 0xdfff) {
-                // The current character is a surrogate.
-                return JSON.stringify(str);
-            }
-            if (
-                point === 0x22 || // '"'
-                point === 0x5c // '\'
-            ) {
-                last === -1 && (last = 0);
-                result += str.slice(last, i) + "\\";
-                last = i;
-            }
-        }
-
-        return (
-            (last === -1 && '"' + str + '"') ||
-            '"' + result + str.slice(last) + '"'
-        );
-    },
+    advanced: $string,
 };
 
 const data: string[] = typia.random<ISmallStrings>().values;
