972538e1fbc6b934cd5ed2382d59551d11e11389
Jeongho Nam <samchon.github@gmail.com>
2023-08-07 19:43:52 +0900

    Enhance #744 - silent setup and enhanced documentation.
    A tiny change does not affect on main setup logic.
    
    So, this commit would be published with next patch.

--- src/executable/setup/CommandExecutor.ts
+++ src/executable/setup/CommandExecutor.ts
@@ -3,7 +3,6 @@ import cp from "child_process";
 export namespace CommandExecutor {
     export const run = (str: string): void => {
         console.log(str);
-        cp.execSync(str, { stdio: "inherit" });
-        // @todo - rollback to ignore
+        cp.execSync(str, { stdio: "ignore" });
     };
 }
