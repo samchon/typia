7817c677277cb3623fc80284d0e5afcb4fd9d74b
Jeongho Nam <samchon.github@gmail.com>
2023-11-24 16:24:36 +0900

    Styling of test program

--- deploy/index.ts
+++ deploy/index.ts
@@ -1,3 +1,4 @@
+import chalk from "chalk";
 import cp from "child_process";
 import fs from "fs";
 import path from "path";
@@ -7,7 +8,7 @@ import { ReplicaPublisher } from "./internal/ReplicaPublisher";
 const execute =
   (label: string) =>
   (command: string): void => {
-    console.log(`\n${label} > ${command}`);
+    console.log(chalk.magentaBright(`\nPS ${label}> ${command}`));
     cp.execSync(command, { stdio: "inherit" });
   };
 
@@ -65,6 +66,13 @@ const test =
     process.chdir(__dirname + "/..");
   };
 
+const title = (label: string): void => {
+  console.log("");
+  console.log("---------------------------------------------------------");
+  console.log(` ${label}`);
+  console.log("---------------------------------------------------------");
+};
+
 const main = (): void => {
   const tag: string | undefined = process.argv[2];
   if (tag === undefined) {
@@ -73,16 +81,19 @@ const main = (): void => {
   }
 
   // BUILD AND TEST
+  title("BUILD THEM MAIN PROGRAM");
   process.chdir(__dirname + "/..");
   execute("typia")("npm run build");
   const version: string = publish("tgz");
 
+  title("TEST AUTOMATION PROGRAM");
   test(version)("test")(["npm run build:actions", "npm start"]);
   test(version)("errors")(["npm start"]);
   test(version)("benchmark")(["npm run build"]);
 
   // MAKE REPLICA REPO
   if (tag !== "tgz") {
+    title("DEPLOY TO NPM");
     publish(tag);
     ["test", "errors", "benchmark"].forEach((name) => test(version)(name)([]));
     ReplicaPublisher.replica(tag);
