f70314b22fd3e548aa1a67337cf983ad46c330cf
Jeongho Nam <samchon.github@gmail.com>
2024-01-15 10:21:30 +0900

    Update website home code block style

--- website/src/components/home/HomeCodeBlock.tsx
+++ website/src/components/home/HomeCodeBlock.tsx
@@ -1,25 +1,28 @@
 import React from "react";
 
+const BRIGHT_BLUE = "rgb(0, 200, 255)";
+const CYAN = "rgb(80, 200, 0)";
+
 const HomeCodeBlock = (props: {
   namespace?: string;
   method: string;
   color?: string;
 }) => (
   <React.Fragment>
-    <span style={{ color: "skyblue" }}>typia</span>
-    {"."}
+    <span style={{ color: BRIGHT_BLUE }}>typia</span>
+    <span style={{ color: "gray" }}>{"."}</span>
     {props.namespace ? (
       <React.Fragment>
-        <span style={{ color: "skyblue" }}>{props.namespace}</span>
+        <span style={{ color: BRIGHT_BLUE }}>{props.namespace}</span>
         <span style={{ color: "gray" }}>{"."}</span>
       </React.Fragment>
     ) : null}
     <span style={{ color: props?.color ?? "yellow" }}>{props.method}</span>
-    <span style={{ color: "rgb(0, 150, 255)" }}>{"<"}</span>
-    <span style={{ color: "cyan" }}>T</span>
-    <span style={{ color: "rgb(0, 150, 255)" }}>{">("}</span>
+    <span style={{ color: BRIGHT_BLUE }}>{"<"}</span>
+    <span style={{ color: CYAN }}>T</span>
+    <span style={{ color: BRIGHT_BLUE }}>{">("}</span>
     <span style={{ color: "gray" }}>{"input"}</span>
-    <span style={{ color: "rgb(0, 150, 255)" }}>{")"}</span>
+    <span style={{ color: BRIGHT_BLUE }}>{")"}</span>
     <span style={{ color: "gray" }}>{";"}</span>
   </React.Fragment>
 );
