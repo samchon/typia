e52ed0d9858aa354effaa95c9302a041f9f2120a
Jeongho Nam <samchon.github@gmail.com>
2024-01-25 01:03:17 +0900

    Change font style of intro page

--- website/src/movies/home/HomeHeroMovie.tsx
+++ website/src/movies/home/HomeHeroMovie.tsx
@@ -2,9 +2,32 @@ import ComputerIcon from "@mui/icons-material/Computer";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import MenuBookIcon from "@mui/icons-material/MenuBook";
 import { Button, Grid, Typography } from "@mui/material";
+import { ReactNode } from "react";
 
 import HomeCodeBlock from "../../components/home/HomeCodeBlock";
 import ProductHeroLayout from "../../components/home/ProductHeroLayout";
+
+const QuickButton = (props: {
+  title: string;
+  href: string;
+  icon: ReactNode;
+  color: "info" | "warning" | "success";
+}) => (
+  <Grid item xs={12} md={4}>
+    <Button
+      color={props.color}
+      variant="outlined"
+      size="large"
+      component="a"
+      href={props.href}
+      startIcon={props.icon}
+      fullWidth
+      style={{ fontFamily: "unset", fontWeight: "bold" }}
+    >
+      {props.title}
+    </Button>
+  </Grid>
+);
 
 const HomeHeroMovie = () => (
   <ProductHeroLayout
@@ -14,65 +37,54 @@ const HomeHeroMovie = () => (
       backgroundPosition: "center",
     }}
   >
-    <Typography color="inherit" align="center" variant="h3">
+    <Typography
+      color="inherit"
+      align="center"
+      variant="h2"
+      fontFamily="fantasy"
+    >
       One Line Magic
     </Typography>
     <Typography
       color="inherit"
       align="center"
-      variant="h6"
+      variant="h5"
+      fontFamily="serif"
       sx={{ mb: 4, mt: { xs: 4, sm: 10 } }}
     >
       No extra schema required.
       <br />
       <br />
       Just fine with pure TypeScript type.
-      <br />
-      <br />
-      <HomeCodeBlock method="assert" />
     </Typography>
     <br />
     <br />
+    <Typography align="center" variant="h5" fontFamily="cursive">
+      <HomeCodeBlock method="assert" inputColor="#CFCFCF" />
+    </Typography>
+    <br />
+    <br />
+    <br />
+    <br />
     <Grid container spacing={2}>
-      <Grid item xs={12} md={4}>
-        <Button
-          color="info"
-          variant="contained"
-          size="large"
-          component="a"
-          href="/docs"
-          startIcon={<MenuBookIcon />}
-          fullWidth
-        >
-          Guide Documents
-        </Button>
-      </Grid>
-      <Grid item xs={12} md={4}>
-        <Button
-          color="warning"
-          variant="contained"
-          size="large"
-          component="a"
-          href="/playground"
-          startIcon={<ComputerIcon />}
-          fullWidth
-        >
-          Playground (Online IDE)
-        </Button>
-      </Grid>
-      <Grid item xs={12} md={4}>
-        <Button
-          color="success"
-          variant="contained"
-          size="large"
-          component="a"
-          href="https://github.com/samchon/typia"
-          startIcon={<GitHubIcon />}
-          fullWidth
-        >
-          Github Repository
-        </Button>
-      </Grid>
+      <QuickButton
+        title="Guide Documents"
+        icon={<MenuBookIcon />}
+        href="/docs"
+        color="info"
+      />
+      <QuickButton
+        title="Playground (Online IDE)"
+        icon={<ComputerIcon />}
+        href="/playground"
+        color="warning"
+      />
+      <QuickButton
+        title="Github Repository"
+        icon={<GitHubIcon />}
+        href="https://github.com/samchon/typia"
+        color="success"
+      />
     </Grid>
   </ProductHeroLayout>
 );
