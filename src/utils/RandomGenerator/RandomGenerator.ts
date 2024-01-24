96c336c76216294730ad307e5b838c0dfab5f532
Jeongho Nam <samchon.github@gmail.com>
2024-01-24 20:12:55 +0900

    Support much more format tags.
    Below format tags are newly added:
    
    - byte
    - password
    - regex
    - hostname
    - uri
    - uri-reference
    - uir-template
    - time
    - duration
    - json-pointer
    - relative-json-pointer

--- src/utils/RandomGenerator/RandomGenerator.ts
+++ src/utils/RandomGenerator/RandomGenerator.ts
@@ -3,28 +3,21 @@ import RandExp from "randexp";
 const ALPHABETS = "abcdefghijklmnopqrstuvwxyz";
 
 /* -----------------------------------------------------------
-      REGULAR
-  ----------------------------------------------------------- */
+  REGULAR
+----------------------------------------------------------- */
 export const boolean = () => Math.random() < 0.5;
-
 export const integer = (min?: number, max?: number) => {
   min ??= 0;
   max ??= 100;
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
-
-export const bigint = (min?: bigint, max?: bigint) => {
-  min ??= BigInt(0);
-  max ??= BigInt(100);
-  return BigInt(integer(Number(min), Number(max)));
-};
-
+export const bigint = (min?: bigint, max?: bigint) =>
+  BigInt(integer(Number(min ?? BigInt(0)), Number(max ?? BigInt(100))));
 export const number = (min?: number, max?: number) => {
   min ??= 0;
   max ??= 100;
   return Math.random() * (max - min) + min;
 };
-
 export const string = (length?: number): string =>
   new Array(length ?? integer(5, 10))
     .fill(0)
@@ -33,14 +26,17 @@ export const string = (length?: number): string =>
 
 export const array = <T>(closure: (index: number) => T, count?: number): T[] =>
   new Array(count ?? length()).fill(0).map((_e, index) => closure(index));
-
 export const pick = <T>(array: T[]): T => array[integer(0, array.length - 1)]!;
-
 export const length = () => integer(0, 3);
+export const pattern = (regex: RegExp): string => new RandExp(regex).gen();
 
 /* -----------------------------------------------------------
-      SECIAL FORMATS
-  ----------------------------------------------------------- */
+  SECIAL FORMATS
+----------------------------------------------------------- */
+// SPECIAL CHARACTERS
+export const byte = () => pattern(/^[\x00-\xff]*$/);
+export const password = () => string(integer(4, 16));
+export const regex = () => pattern(/[^\\]\Z/);
 export const uuid = () =>
   "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
     const r = (Math.random() * 16) | 0;
@@ -48,27 +44,49 @@ export const uuid = () =>
     return v.toString(16);
   });
 
+// ADDRESSES
 export const email = () => `${string(10)}@${string(10)}.${string(3)}`;
-
-export const url = () => `https://${string(10)}.${string(3)}`;
-
 export const ipv4 = () => array(() => integer(0, 255), 4).join(".");
-
 export const ipv6 = (): string =>
   array(() => integer(0, 65535).toString(16), 8).join(":");
+export const uri = () => url();
+export const uriReference = () => url();
+export const uriTemplate = () => url();
+export const url = () => `https://${string(10)}.${string(3)}`;
 
-export const pattern = (regex: RegExp): string => new RandExp(regex).gen();
-
-export const date = (min?: number, max?: number) => {
-  min ??= 0;
-  max ??= Date.now() * 2;
-  return new Date(number(min, max)).toISOString().substring(0, 10);
+// TIMESTAMPS
+export const datetime = (min?: number, max?: number) =>
+  new Date(
+    number(min ?? Date.now() - 30 * DAY, max ?? Date.now() + 7 * DAY),
+  ).toISOString();
+export const date = (min?: number, max?: number) =>
+  new Date(number(min ?? 0, max ?? Date.now() * 2))
+    .toISOString()
+    .substring(0, 10);
+export const time = () =>
+  new Date(number(0, DAY)).toISOString().substring(11, 23);
+export const duration = () => {
+  const period: string = durate([
+    ["Y", integer(0, 100)],
+    ["M", integer(0, 12)],
+    ["D", integer(0, 31)],
+  ]);
+  const time: string = durate([
+    ["H", integer(0, 24)],
+    ["M", integer(0, 60)],
+    ["S", integer(0, 60)],
+  ]);
+  if (period.length + time.length === 0) return "PT0S";
+  return `P${period}${time.length ? "T" : ""}${time}`;
 };
 
-export const datetime = (min?: number, max?: number) => {
-  min ??= Date.now() - 30 * DAY;
-  max ??= Date.now() + 7 * DAY;
-  return new Date(number(min, max)).toISOString();
-};
+// POINTERS
+export const jsonPointer = () => `/components/schemas/${string(10)}`;
+export const relativeJsonPointer = () => `${integer(0, 10)}#`;
 
 const DAY = 86400000;
+const durate = (elements: [string, number][]) =>
+  elements
+    .filter(([_unit, value]) => value !== 0)
+    .map(([unit, value]) => `${value}${unit}`)
+    .join("");
