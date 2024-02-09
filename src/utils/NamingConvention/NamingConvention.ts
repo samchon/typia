232bddfb37f44cb2c3ffdb7d100b442d8b2b72c8
Jeongho Nam <samchon.github@gmail.com>
2024-02-10 00:13:17 +0900

    Fix #958 - wrong camel case when `_` starting variable comes.
    When underscore started variable comes, `typia.notations.camel<T>()` function had failed to perform the proper camcel case conversion. For example, `__id` property had been converted to the `__Id`.
    
    This PR fixes the bug, so that camel and pascal case conversion becomes exact.
    
    ```typescript
    interface Lower {
      _id: string;
      __id: string;
      ___id_something: string;
      ___idNothing: string;
    }
    ```

--- src/utils/NamingConvention/NamingConvention.ts
+++ src/utils/NamingConvention/NamingConvention.ts
@@ -27,51 +27,60 @@ export function snake(str: string): string {
 }
 
 export function camel(str: string): string {
-  str = removeSnake(str);
-  if (str.length === 0) return str;
-  else if (str[0] === str[0]!.toUpperCase())
-    return str[0]!.toLowerCase() + str.substring(1);
-  else if (str[0] === "_" && str.length > 1 && str[1] === str[1]!.toLowerCase())
-    return "_" + str[1]!.toUpperCase() + str.substring(2);
-  else return str;
+  return unsnake((str: string) => {
+    if (str.length === 0) return str;
+    else if (str[0] === str[0]!.toUpperCase())
+      return str[0]!.toLowerCase() + str.substring(1);
+    else return str;
+  })(str);
 }
 
 export function pascal(str: string): string {
-  str = removeSnake(str);
-  if (str.length === 0) return str;
-  else if (str[0] === str[0]!.toLowerCase())
-    return str[0]!.toUpperCase() + str.substring(1);
-  else if (str[0] === "_" && str.length > 1 && str[1] === str[1]!.toLowerCase())
-    return "_" + str[1]!.toUpperCase() + str.substring(2);
-  else return str;
+  return unsnake((str: string) => {
+    if (str.length === 0) return str;
+    else if (str[0] === str[0]!.toLowerCase())
+      return str[0]!.toUpperCase() + str.substring(1);
+    else return str;
+  })(str);
 }
 
-const removeSnake = (str: string): string => {
-  const indexes: [number, number][] = [];
-  for (let i: number = 0; i < str.length; i++) {
-    const ch: string = str[i]!;
-    if (ch !== "_") continue;
-
-    const last = indexes[indexes.length - 1];
-    if (last === undefined || last[0] + last[1] !== i) indexes.push([i, 1]);
-    else ++last[1];
-  }
-  if (indexes.length === 0) return str;
-
-  let ret: string = "";
-  for (let i: number = 0; i < indexes.length; i++) {
-    const [first] = indexes[i]!;
-    if (i === 0)
-      if (first === 0) ret += "_";
-      else ret += str.substring(0, first);
-    else {
-      const [prevFirst, prevLength] = indexes[i - 1]!;
-      const piece: string = str.substring(prevFirst + prevLength, first);
-      if (piece.length) ret += StringUtil.capitalize(piece);
+const unsnake =
+  (escaper: (str: string) => string) =>
+  (str: string): string => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    let prefix: string = "";
+    for (let i: number = 0; i < str.length; i++) {
+      if (str[i] === "_") prefix += "_";
+      else break;
     }
-  }
-  const last = indexes[indexes.length - 1]!;
-  const piece: string = str.substring(last[0] + last[1]);
-  if (last.length) ret += StringUtil.capitalize(piece);
-  return ret;
-};
+    if (prefix.length !== 0) str = str.substring(prefix.length);
+
+    const indexes: [number, number][] = [];
+    for (let i: number = 0; i < str.length; i++) {
+      const ch: string = str[i]!;
+      if (ch !== "_") continue;
+
+      const last = indexes[indexes.length - 1];
+      if (last === undefined || last[0] + last[1] !== i) indexes.push([i, 1]);
+      else ++last[1];
+    }
+    if (indexes.length === 0) return prefix + escaper(str);
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    let ret: string = "";
+    for (let i: number = 0; i < indexes.length; i++) {
+      const [first] = indexes[i]!;
+      if (i === 0)
+        if (first === 0) ret += "_";
+        else ret += str.substring(0, first);
+      else {
+        const [prevFirst, prevLength] = indexes[i - 1]!;
+        const piece: string = str.substring(prevFirst + prevLength, first);
+        if (piece.length) ret += StringUtil.capitalize(piece);
+      }
+    }
+    const last = indexes[indexes.length - 1]!;
+    const piece: string = str.substring(last[0] + last[1]);
+    if (last.length) ret += StringUtil.capitalize(piece);
+    return prefix + escaper(ret);
+  };
