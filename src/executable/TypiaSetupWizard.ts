74cd331cd332eba59d263ed6c929a410001d5176
Jeongho Nam <samchon.github@gmail.com>
2024-03-14 15:46:13 +0900

    Clos #868 and #1004 - supports Array extended interface type.
    Looking at prisma generated schema definition, `JsonArray` interface type is extending `Array<JsonValue` type. In that case, TypeScript compiler considers it as an object type with `length` and dynamic number key properties. At that time, I'd requested `prisma` to fix the wrong type - prisma/prisma#21921.
    
    ```typescript
    // PRISMA TYPES
    export type JsonValue =
      | string
      | number
      | boolean
      | JsonObject
      | JsonArray
      | null;
    export interface JsonObject {
      [key: string]: JsonValue | undefined;
    }
    export interface JsonArray extends Array<JsonValue> {}
    
    // TYPESCRIPT COMPILER IDENTIFIES IT AS AN OBJECT
    type JsonArray = {
      length: number;
      [n: number]: JsonValue;
    };
    ```
    
    By the way, `redis` is doing the same crazy thing that inheriting the `Array<T>` type like below.
    
    ```typescript
    interface RedisJSONArray extends Array<RedisJSON> {}
    interface RedisJSONObject {
      [key: string]: RedisJSON;
      [key: number]: RedisJSON;
    }
    type RedisJSON = null | boolean | number | string | Date | RedisJSONArray | RedisJSONObject;
    ```
    
    Even though inheriting Array type is crazy behavior and nonsensible within framework of the compiler theory, such notation had been used too much a lot. Thus, I've decided to support such crazy expression by hard coding.
    
    From now on, Array inherited interface type works not as object, but as array.

--- src/executable/TypiaSetupWizard.ts
+++ src/executable/TypiaSetupWizard.ts
@@ -23,7 +23,7 @@ export namespace TypiaSetupWizard {
     // INSTALL TYPESCRIPT COMPILERS
     pack.install({ dev: true, modulo: "ts-patch", version: "latest" });
     pack.install({ dev: true, modulo: "ts-node", version: "latest" });
-    pack.install({ dev: true, modulo: "typescript", version: "5.3.2" });
+    pack.install({ dev: true, modulo: "typescript", version: "5.4.2" });
     args.project ??= (() => {
       const runner: string = pack.manager === "npm" ? "npx" : pack.manager;
       CommandExecutor.run(`${runner} tsc --init`);
