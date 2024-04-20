c177e10f7dfa0536aa02b429c1ad4e39df305ec3
Jeongho Nam <samchon.github@gmail.com>
2024-04-20 22:48:31 +0900

    Upgrade `@samchon/openapi` and revive benchmark program.

--- benchmark/src/programs/validate/ajv/createValidateAjvBenchmarkProgram.ts
+++ benchmark/src/programs/validate/ajv/createValidateAjvBenchmarkProgram.ts
@@ -1,24 +1,18 @@
 import Ajv from "ajv";
 import { IJsonApplication } from "typia";
 
+import { AjvFactory } from "../../../internal/AjvFactory";
 import { createValidateBenchmarkProgram } from "../createValidateBenchmarkProgram";
 
 export const createValidateAjvBenchmarkProgram = (
   app: IJsonApplication<"3.0">,
 ) => {
-  const program = new Ajv({
-    schemas: Object.entries(app.components.schemas ?? {}).map(
-      ([key, value]) => ({
-        ...value,
-        $id: `#/components/schemas/${key}`,
-      }),
-    ),
-    strict: true,
-    strictNumbers: false,
-    allErrors: true,
-  });
   try {
-    const validate = program.compile(app.schemas[0]);
+    const validate = AjvFactory.create({
+      strict: true,
+      strictNumbers: false,
+      allErrors: true,
+    })(app);
     createValidateBenchmarkProgram((input) => {
       validate(input);
       return validate.errors ?? [];
