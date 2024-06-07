ef488ad43edd00a09aa66d4dbe364c8686c1e243
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-06-07 14:33:19 +0100

    feat(rollup.config): support multiple formats and preserve modules
    The rollup configuration has been updated to support both ESM and CJS
    formats. The 'preserveModules' option is enabled to keep the directory
    structure of the source files in the output. The input files are now
    determined dynamically using glob, which allows for multiple entry points.

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,50 @@
 const typescript = require("@rollup/plugin-typescript");
 const terser = require("@rollup/plugin-terser");
 
-module.exports = {
-  input: "./src/index.ts",
-  output: {
-    dir: "lib",
-    format: "esm",
-    entryFileNames: "[name].mjs",
-    sourcemap: true,
-  },
-  plugins: [
-    typescript({
-      tsconfig: "tsconfig.json",
-      module: "ES2020",
-      target: "ES2020",
-    }),
-    terser({
-      format: {
-        comments: "some",
-        beautify: true,
-        ecma: "2020",
-      },
-      compress: false,
-      mangle: false,
-      module: true,
-    }),
-  ],
+const glob = require("glob");
+
+const input = glob.sync("src/**/*.ts");
+
+const outputConfig = {
+  dir: "lib",
+  sourcemap: true,
+  preserveModules: true,
 };
+
+const plugins = [
+  typescript({
+    tsconfig: "tsconfig.json",
+    module: "ES2020",
+    target: "ES2020",
+  }),
+  terser({
+    format: {
+      comments: "some",
+      beautify: true,
+      ecma: "2020",
+    },
+    compress: false,
+    mangle: false,
+    module: true,
+  }),
+];
+
+module.exports = [
+  {
+    input,
+    output: {
+      ...outputConfig,
+      entryFileNames: "[name].mjs",
+      format: "esm",
+    },
+    plugins,
+  },
+  {
+    input,
+    output: {
+      ...outputConfig,
+      format: "cjs",
+    },
+    plugins,
+  },
+];
