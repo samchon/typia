02761b17248af2462f4a80f678b66acc097b8cd3
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-06-26 17:09:57 +0100

    refactor: remove terser and update TypeScript target
    The terser plugin has been removed from the project as it's no longer
    needed. The TypeScript target and module have been updated to "ESNext"
    to align with the latest ECMAScript features.

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 const typescript = require("@rollup/plugin-typescript");
-const terser = require("@rollup/plugin-terser");
 const nodeResolve = require("@rollup/plugin-node-resolve");
 const commomnjs = require("@rollup/plugin-commonjs");
 
@@ -18,18 +17,8 @@ module.exports = {
     commomnjs(),
     typescript({
       tsconfig: "tsconfig.json",
-      module: "ES2020",
-      target: "ES2020",
-    }),
-    terser({
-      format: {
-        comments: "some",
-        beautify: true,
-        ecma: "2020",
-      },
-      compress: false,
-      mangle: false,
-      module: true,
+      module: "ESNext",
+      target: "ESNext",
     }),
   ],
 };
