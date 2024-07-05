b40215117b49dc952631eca2a4f7bdda0c69da35
Jeongho Nam <samchon.github@gmail.com>
2024-07-05 20:18:22 +0900

    Close #1149: `IJsonSchema.IOneOf.discriminator`.
    `typia.json.application<[Types]>()` starts supporting `IJsonSchema.IOneOf.discriminator` property.
    
    This is the spec of the `IJsonSchema.IOneOf.discriminator`.
    
    https://github.com/samchon/openapi/blob/0cf6d4e5cd211200b2a7f3a30a56ff86f49e4d9d/src/OpenApi.ts#L983-L1015
    
    ```typescript
        export interface IOneOf<Schema extends IJsonSchema = IJsonSchema>
          extends __IAttribute {
          /**
           * List of the union types.
           */
          oneOf: Exclude<Schema, IJsonSchema.IOneOf>[];
    
          /**
           * Discriminator info of the union type.
           */
          discriminator?: IOneOf.IDiscriminator;
        }
        export namespace IOneOf {
          /**
           * Discriminator info of the union type.
           */
          export interface IDiscriminator {
            /**
             * Property name for the discriminator.
             */
            propertyName: string;
    
            /**
             * Mapping of the discriminator value to the schema name.
             *
             * This property is valid only for {@link IReference} typed
             * {@link IOneOf.oneof} elements. Therefore, `key` of `mapping` is
             * the discriminator value, and `value` of `mapping` is the
             * schema name like `#/components/schemas/SomeObject`.
             */
            mapping?: Record<string, string>;
          }
        }
    ```

---
+++ src/programmers/internal/application_union_discriminator.ts
@@ -0,0 +1,35 @@
+import { OpenApi } from "@samchon/openapi";
+
+import { Metadata } from "../../schemas/metadata/Metadata";
+
+import { UnionPredicator } from "../helpers/UnionPredicator";
+
+export const application_union_discriminator = (
+  meta: Metadata,
+): OpenApi.IJsonSchema.IOneOf.IDiscriminator | undefined => {
+  if (
+    meta.size() === 0 ||
+    meta.size() !== meta.objects.length ||
+    meta.objects.some((o) => o._Is_literal()) === true
+  )
+    return undefined;
+  const specialized: UnionPredicator.ISpecialized[] = UnionPredicator.object(
+    meta.objects,
+  );
+  const meet: boolean =
+    specialized.length === meta.objects.length &&
+    specialized.every(
+      (s) => s.property.key.isSoleLiteral() && s.property.value.isSoleLiteral(),
+    ) &&
+    new Set(specialized.map((s) => s.property.key.getSoleLiteral())).size === 1;
+  if (meet === false) return undefined;
+  return {
+    propertyName: specialized[0]!.property.key.getSoleLiteral()!,
+    mapping: Object.fromEntries(
+      specialized.map((s) => [
+        s.property.value.getSoleLiteral()!,
+        `#/components/schemas/${s.object.name}`,
+      ]),
+    ),
+  };
+};
