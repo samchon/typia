4e27c9b9d12d364a2c059aa95e558b329ec3bae9
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-08 14:34:39 +0100

    feat: add orphan Type to `IJSONApplication` to save types

--- src/schemas/json/IJsonApplication.ts
+++ src/schemas/json/IJsonApplication.ts
@@ -1,14 +1,18 @@
 import type { OpenApi, OpenApiV3 } from "@samchon/openapi";
 
-export type IJsonApplication<Version extends "3.0" | "3.1" = "3.1"> =
-  Version extends "3.0" ? IJsonApplication.IV3_0 : IJsonApplication.IV3_1;
+declare const tag: unique symbol;
+
+export type IJsonApplication<Version extends "3.0" | "3.1" = "3.1", T = unknown[]> =
+  Version extends "3.0" ? IJsonApplication.IV3_0<T> : IJsonApplication.IV3_1<T>;
 export namespace IJsonApplication {
-  export interface IV3_0 {
+  export interface IV3_0<T> {
+    [tag]: T;
     version: "3.0";
     schemas: OpenApiV3.IJsonSchema[];
     components: OpenApiV3.IComponents;
   }
-  export interface IV3_1 {
+  export interface IV3_1<T> {
+    [tag]: T;
     version: "3.1";
     components: OpenApi.IComponents;
     schemas: OpenApi.IJsonSchema[];
