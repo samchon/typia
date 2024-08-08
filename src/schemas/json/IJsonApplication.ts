7309a028b7e9beee0a295d7fdbb0bd7759b55bef
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-08 14:51:19 +0100

    fix: type for openapi generating functions

--- src/schemas/json/IJsonApplication.ts
+++ src/schemas/json/IJsonApplication.ts
@@ -1,17 +1,17 @@
 import type { OpenApi, OpenApiV3 } from "@samchon/openapi";
 
-declare const tag: unique symbol;
+export declare const tag: unique symbol;
 
 export type IJsonApplication<Version extends "3.0" | "3.1" = "3.1", Types = unknown[]> =
   Version extends "3.0" ? IJsonApplication.IV3_0<Types> : IJsonApplication.IV3_1<Types>;
 export namespace IJsonApplication {
-  export interface IV3_0<Types> {
+  export interface IV3_0<Types = unknown[]> {
     [tag]: Types;
     version: "3.0";
     schemas: OpenApiV3.IJsonSchema[];
     components: OpenApiV3.IComponents;
   }
-  export interface IV3_1<Types> {
+  export interface IV3_1<Types = unknown[]> {
     [tag]: Types;
     version: "3.1";
     components: OpenApi.IComponents;
