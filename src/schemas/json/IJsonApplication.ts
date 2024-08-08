13bf490426bfb8d505d4335033cea2a0199280c6
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-08 14:41:54 +0100

    feat: rename T to Types

--- src/schemas/json/IJsonApplication.ts
+++ src/schemas/json/IJsonApplication.ts
@@ -2,17 +2,17 @@ import type { OpenApi, OpenApiV3 } from "@samchon/openapi";
 
 declare const tag: unique symbol;
 
-export type IJsonApplication<Version extends "3.0" | "3.1" = "3.1", T = unknown[]> =
-  Version extends "3.0" ? IJsonApplication.IV3_0<T> : IJsonApplication.IV3_1<T>;
+export type IJsonApplication<Version extends "3.0" | "3.1" = "3.1", Types = unknown[]> =
+  Version extends "3.0" ? IJsonApplication.IV3_0<Types> : IJsonApplication.IV3_1<Types>;
 export namespace IJsonApplication {
-  export interface IV3_0<T> {
-    [tag]: T;
+  export interface IV3_0<Types> {
+    [tag]: Types;
     version: "3.0";
     schemas: OpenApiV3.IJsonSchema[];
     components: OpenApiV3.IComponents;
   }
-  export interface IV3_1<T> {
-    [tag]: T;
+  export interface IV3_1<Types> {
+    [tag]: Types;
     version: "3.1";
     components: OpenApi.IComponents;
     schemas: OpenApi.IJsonSchema[];
