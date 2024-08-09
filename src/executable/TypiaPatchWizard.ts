6a61e7d88e58dfa8c566e4af6f80b1de1e5283c8
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-09 23:13:08 +0100

    feat: replace patch wizard with citty library

--- src/executable/TypiaPatchWizard.ts
+++ src/executable/TypiaPatchWizard.ts
@@ -1,10 +1,13 @@
-import fs from "fs";
+import fs from "node:fs";
+import { defineCommand } from "citty";
 
-export namespace TypiaPatchWizard {
-  export const main = async (): Promise<void> => {
-    console.log("----------------------------------------");
-    console.log(" Typia Setup Wizard");
-    console.log("----------------------------------------");
+const FROM_WITH_COMMENT = `var defaultJSDocParsingMode = 2 /* ParseForTypeErrors */`;
+const TO_WITH_COMMENT = `var defaultJSDocParsingMode = 0 /* ParseAll */`;
+const FROM_ONLY = `var defaultJSDocParsingMode = 2`;
+const TO_ONLY = `var defaultJSDocParsingMode = 0`;
+
+export const patchCommand = defineCommand({
+  async setup(){
     console.log(
       [
         `Since TypeScript v5.3 update, "tsc" no more parses JSDoc comments.`,
@@ -14,29 +17,26 @@ export namespace TypiaPatchWizard {
         `This is a temporary feature of "typia", and it would be removed when "ts-patch" being updated.`,
       ].join("\n"),
     );
-
+  },
+  async run() {
     await patch();
-  };
+  },
+});
 
-  export const patch = async (): Promise<void> => {
-    const location: string = require.resolve("typescript/lib/tsc.js");
-    const content: string = await fs.promises.readFile(location, "utf8");
-    if (content.indexOf(FROM_WITH_COMMENT) !== -1)
-      await fs.promises.writeFile(
-        location,
-        content.replace(FROM_WITH_COMMENT, TO_WITH_COMMENT),
-        "utf8",
-      );
+export async function patch(): Promise<void> {
+  const location: string = require.resolve("typescript/lib/tsc.js");
+  const content: string = await fs.promises.readFile(location, "utf8");
+  if (content.indexOf(FROM_WITH_COMMENT) !== -1)
+    await fs.promises.writeFile(
+      location,
+      content.replace(FROM_WITH_COMMENT, TO_WITH_COMMENT),
+      "utf8"
+    );
     else if (content.indexOf(FROM_ONLY) !== -1)
       await fs.promises.writeFile(
         location,
         content.replace(FROM_ONLY, TO_ONLY),
-        "utf8",
+        "utf8"
       );
-  };
 }
 
-const FROM_WITH_COMMENT = `var defaultJSDocParsingMode = 2 /* ParseForTypeErrors */`;
-const TO_WITH_COMMENT = `var defaultJSDocParsingMode = 0 /* ParseAll */`;
-const FROM_ONLY = `var defaultJSDocParsingMode = 2`;
-const TO_ONLY = `var defaultJSDocParsingMode = 0`;
---
+++ src/executable/utils.ts
@@ -0,0 +1,5 @@
+export function wizard(): void {
+    console.log("----------------------------------------");
+    console.log(" Typia Setup Wizard");
+    console.log("----------------------------------------");
+}
