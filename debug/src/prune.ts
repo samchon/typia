651de833e1eaa467eb2246a6c1be0ef1db3594fb
Jeongho Nam <samchon.github@gmail.com>
2024-08-11 16:36:36 +0900

    Change `MetadataAtomic.toJSON()` policy to allow `bigint` type's `tag` property.

--- debug/src/prune.ts
+++
@@ -1,7 +0,0 @@
-import typia, { tags } from "typia";
-
-interface DynamicTag {
-  [key: number & tags.Minimum<0> & tags.ExclusiveMaximum<10>]: number;
-  [key: string & tags.Format<"uuid">]: string;
-}
-typia.misc.createPrune<DynamicTag>();
