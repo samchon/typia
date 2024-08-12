71ab0e07228ec0ea95713d2d621bfa9f2a6f276a
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-12 19:32:05 +0100

    style: make named function

--- src/executable/utils/command.ts
+++ src/executable/utils/command.ts
@@ -1,6 +1,6 @@
 import cp from "child_process";
 
-export const run = (str: string): void => {
-  console.log(`\n$ ${str}`);
-  cp.execSync(str, { stdio: "inherit" });
+export function run(str: string): void {
+    console.log(`\n$ ${str}`);
+    cp.execSync(str, { stdio: "inherit" });
 };
