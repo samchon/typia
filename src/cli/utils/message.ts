ed7cdc7148366c10f36974719abb7ed862e87cbf
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-27 15:31:18 +0100

    feat(cli/utils): create message fucnctions

---
+++ src/cli/utils/message.ts
@@ -0,0 +1,13 @@
+import { logger } from "./logger";
+
+/**
+ * throw an error message and exit the process
+ */
+export function bail(message: string): never {
+  logger.error(message);
+  process.exit(1);
+}
+
+export function wizard(): void {
+  logger.box("Typia Setup Wizard");
+}
