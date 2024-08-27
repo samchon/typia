3014475e3bb206e7030223d1015c912a51a829c4
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-27 15:41:28 +0100

    feat(cli): add patch command

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,10 +1,19 @@
 import { cli as cleye } from 'cleye'
+import { patch } from './subcommands/patch';
 
 export async function cli(){
   const argv = cleye({
     name: "typia",
     version: "1.0.0",
     description: "CLI for Typia operations",
+
+    commands: [
+      patch,
+    ],
+
   })
+
+  const { flags } = argv;
+
   console.log(argv)
 }
