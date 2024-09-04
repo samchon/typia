f5dfb39126756c73ca829f45ac3a451d21809499
Jeongho Nam <samchon.github@gmail.com>
2024-09-04 12:19:47 +0900

    Revive sitemap.xml

--- website/build/deploy.js
+++ website/build/deploy.js
@@ -1,7 +1,18 @@
 const cp = require("child_process");
+const fs = require("fs");
 const deploy = require("gh-pages");
 
 cp.execSync("npm run build", { stdio: "inherit" });
+
+for (const file of fs.readdirSync(`${__dirname}/../public`))
+  if (
+    file === "robots.txt" ||
+    (file.startsWith("sitemap") && file.endsWith(".xml"))
+  )
+    fs.copyFileSync(
+      `${__dirname}/../public/${file}`,
+      `${__dirname}/../out/${file}`,
+    );
 
 deploy.publish(
   "out",
