4fb86ab32921712f67d98a6549c1b639cf63c459
Jeongho Nam <samchon.github@gmail.com>
2024-10-04 02:26:03 +0900

    Filling internal functions

---
+++ src/internal/$jsonStringifyOfNumber.ts
@@ -0,0 +1,12 @@
+import { TypeGuardError } from "../TypeGuardError";
+
+export const $$jsonStringifyOfNumber = (value: number): number => {
+  if (isFinite(value) === false)
+    throw new TypeGuardError({
+      method: "typia.json.stringify",
+      expected: "number",
+      value,
+      message: "Error on typia.json.stringify(): infinite or not a number.",
+    });
+  return value;
+};
