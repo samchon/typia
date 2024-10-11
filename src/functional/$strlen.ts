07f2d33085e686c8cd4dab499799380f74e7198d
Jeongho Nam <samchon.github@gmail.com>
2024-10-11 18:11:31 +0900

    Fix #1036: emoji case in the `protobuf`

--- src/functional/$strlen.ts
+++ src/functional/$strlen.ts
@@ -1,7 +1,1 @@
-export const $strlen = (s: string): number => {
-  let b: number;
-  let i: number;
-  let c: number;
-  for (b = i = 0; (c = s.charCodeAt(i++)); b += c >> 11 ? 3 : c >> 7 ? 2 : 1);
-  return b;
-};
+export const $strlen = (str: string): number => new Blob([str]).size;
