678f8edf0848b26d6d816b2fed25ae546d48dd1c
Jeongho Nam <samchon.github@gmail.com>
2024-11-28 01:52:11 +0900

    Fix test program level bug.

--- test/src/features/issues/test_issue_1390_llm_config_argument.ts
+++ test/src/features/issues/test_issue_1390_llm_config_argument.ts
@@ -5,18 +5,14 @@ import { TestValidator } from "../../helpers/TestValidator";
 import { ArrayRecursive } from "../../structures/ArrayRecursive";
 
 export const test_issue_1390_llm_config_argument = (): void => {
-  TestValidator.equals("false")(
+  TestValidator.equals("reference false")(
     keys(($defs) =>
-      typia.llm.schema<ArrayRecursive, "chatgpt", { reference: false }>({
-        $defs,
-      }),
+      typia.llm.schema<ArrayRecursive, "chatgpt", { reference: false }>($defs),
     ),
   )(["ArrayRecursive.ICategory"]);
-  TestValidator.equals("false")(
+  TestValidator.equals("reference true")(
     keys(($defs) =>
-      typia.llm.schema<ArrayRecursive, "chatgpt", { reference: true }>({
-        $defs,
-      }),
+      typia.llm.schema<ArrayRecursive, "chatgpt", { reference: true }>($defs),
     ),
   )(["ArrayRecursive.ICategory", "ArrayRecursive.ITimestamp"]);
 };
@@ -25,6 +21,9 @@ const keys = (
   factory: ($defs: Record<string, any>) => IChatGptSchema,
 ): string[] => {
   const $defs: Record<string, any> = {};
-  const schema = factory($defs);
+  const schema: IChatGptSchema = factory($defs);
+  TestValidator.equals("schema")(schema)({
+    $ref: "#/$defs/ArrayRecursive.ICategory",
+  });
   return Object.keys($defs);
 };
