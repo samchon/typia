75e2def591b58c52653423633a5793866000e225
Jeongho Nam <samchon.github@gmail.com>
2024-12-12 02:32:04 +0900

    Refactor README and index.html

--- website/src/components/home/HomeLayout.tsx
+++ website/src/components/home/HomeLayout.tsx
@@ -1,11 +1,16 @@
+import { Bleed } from "nextra-theme-docs";
+
 const HomeLayout = (props: React.HTMLAttributes<HTMLDivElement>) => (
-  <div
-    style={{
-      marginLeft: "-1.5rem",
-      marginRight: "-1.5rem",
-    }}
-  >
-    {props.children}
-  </div>
+  <Bleed full>
+    <div
+      style={{
+        // marginLeft: "-1.5rem",
+        marginLeft: "calc(50% - 50vw)",
+        marginRight: "-1.5rem",
+      }}
+    >
+      {props.children}
+    </div>
+  </Bleed>
 );
 export default HomeLayout;
