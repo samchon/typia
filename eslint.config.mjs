2e4059454ede91df2af2f0cc1bf3eb80a35f6670
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-02-02 23:25:22 +0000

    feat(eslint): enable consistent-type-imports rule
    https://typescript-eslint.io/rules/consistent-type-imports/

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -31,7 +31,7 @@ export default [...compat.extends("plugin:@typescript-eslint/recommended"), {
     files: ["src/**/*.ts", "test/**/*.ts", "benchmark/**/*.ts"],
 
     rules: {
-        "@typescript-eslint/consistent-type-definitions": "off",
+        "@typescript-eslint/consistent-type-imports": "error", // enforce consistent usage of type imports
         "@typescript-eslint/no-empty-function": "off",
         "@typescript-eslint/no-empty-interface": "off",
         "@typescript-eslint/no-explicit-any": "off",
@@ -44,6 +44,6 @@ export default [...compat.extends("plugin:@typescript-eslint/recommended"), {
         "@typescript-eslint/no-require-imports": "off",
         "@typescript-eslint/no-empty-object-type": "off",
         "@typescript-eslint/no-unsafe-function-type": "off",
-        "@typescript-eslint/no-deprecated": "error"
+        "@typescript-eslint/no-deprecated": "error",
     },
 }];
