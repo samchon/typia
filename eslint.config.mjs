37c033c63766d35bd27681a3be230aebfab60e7a
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-02-02 23:18:47 +0000

    feat(eslint): migrate to eslint v9

---
+++ eslint.config.mjs
@@ -0,0 +1,50 @@
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import deprecation from "eslint-plugin-deprecation";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...compat.extends("plugin:@typescript-eslint/recommended"), {
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+        deprecation,
+    },
+
+    languageOptions: {
+        parser: tsParser,
+        ecmaVersion: 5,
+        sourceType: "script",
+
+        parserOptions: {
+            project: ["tsconfig.json"],
+        },
+    },
+}, {
+    files: ["src/**/*.ts", "test/**/*.ts", "benchmark/**/*.ts"],
+
+    rules: {
+        "@typescript-eslint/consistent-type-definitions": "off",
+        "@typescript-eslint/no-empty-function": "off",
+        "@typescript-eslint/no-empty-interface": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-inferrable-types": "off",
+        "@typescript-eslint/no-namespace": "off",
+        "@typescript-eslint/no-non-null-assertion": "off",
+        "@typescript-eslint/no-unused-vars": "off",
+        "@typescript-eslint/no-var-requires": "off",
+        "@typescript-eslint/no-floating-promises": "error",
+        "@typescript-eslint/no-require-imports": "off",
+        "@typescript-eslint/no-empty-object-type": "off",
+        "@typescript-eslint/no-unsafe-function-type": "off",
+    },
+}];
