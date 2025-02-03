659cad0124611421521dc71b312f4e212c906ae5
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-02-03 22:40:08 +0000

    feat(eslint): rewrite for flat config using typescript-eslint
    https://typescript-eslint.io/getting-started/
    https://typescript-eslint.io/getting-started/typed-linting/

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,57 +1,45 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
 import gitignore from 'eslint-config-flat-gitignore'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
 
-export default [
-    gitignore(),
-    ...compat.extends("plugin:@typescript-eslint/recommended"),
-    {
-        plugins: {
-            "@typescript-eslint": typescriptEslint,
-        },
-
-        languageOptions: {
-            parser: tsParser,
-            ecmaVersion: 5,
-            sourceType: "script",
-
-            parserOptions: {
-                project: ["tsconfig.json"],
-            },
-        },
+export default tseslint.config(
+  gitignore(),
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
-    {
-        rules: {
-            "@typescript-eslint/consistent-type-definitions": "off",
-            "@typescript-eslint/no-empty-function": "off",
-            "@typescript-eslint/no-empty-interface": "off",
-            "@typescript-eslint/no-explicit-any": "off",
-            "@typescript-eslint/no-inferrable-types": "off",
-            "@typescript-eslint/no-namespace": "off",
-            "@typescript-eslint/no-non-null-assertion": "off",
-            "@typescript-eslint/no-unused-vars": "off",
-            "@typescript-eslint/no-var-requires": "off",
-            "@typescript-eslint/no-floating-promises": "error",
-            "@typescript-eslint/no-require-imports": "off",
-            "@typescript-eslint/no-empty-object-type": "off",
-            "@typescript-eslint/no-unsafe-function-type": "off",
-            "@typescript-eslint/no-wrapper-object-types": "off",
-            "@typescript-eslint/no-deprecated": "error",
-            "prefer-const": ["error"],
-            "no-var": ["error"],
-            "semi": ["error", "always"],
-        },
+  },
+  {
+    rules: {
+      "prefer-const": ["error"],
+      "no-var": ["error"],
+      "semi": ["error", "always"],
     }
-];
+  },
+  {
+    rules: {
+      "@typescript-eslint/consistent-type-definitions": "off",
+      "@typescript-eslint/no-empty-function": "off",
+      "@typescript-eslint/no-empty-interface": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-inferrable-types": "off",
+      "@typescript-eslint/no-namespace": "off",
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-var-requires": "off",
+      "@typescript-eslint/no-floating-promises": "error",
+      "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-unsafe-function-type": "off",
+      "@typescript-eslint/no-wrapper-object-types": "off",
+      "@typescript-eslint/no-deprecated": "error",
+    },
+  }
+);
