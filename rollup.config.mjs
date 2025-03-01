4c1c91d5f3aba74f93ea9ed9b32846b0afa11972
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-03-01 14:52:03 +0000

    fix(rollup): throw error if we failed external deps

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -13,21 +13,9 @@ export default {
     sourcemap: true,
     entryFileNames: (chunkInfo) => {
       const ext = `mjs`;
-      const externalDir = `_external`;
       const nodeModulesDir = `node_modules`;
       if (chunkInfo.name.includes(nodeModulesDir)) {
-        /** replace / to _ and the last part of the path is the file name */
-        const nameSplit = chunkInfo.name.split("/");
-        const chunkName = path.join(
-          externalDir,
-          nameSplit.slice(0, -1).join("_"),
-          nameSplit.at(-1),
-        );
-        console.table({
-          before: chunkInfo.name,
-          after: chunkName,
-        });
-        return `${chunkName}.${ext}`;
+        throw new Error(`Invalid chunk name: ${chunkInfo.name}`);
       }
       return `[name].${ext}`;
     },
