75f9eb82c5d94c3addf795e13f3a3559cf325743
Jeongho Nam <samchon.github@gmail.com>
2025-04-06 00:04:54 +0900

    Emend exclusive constraints

--- test/src/debug/schema.ts
+++ test/src/debug/schema.ts
@@ -2,4 +2,8 @@ import typia, { tags } from "typia";
 
 console.log(
   typia.llm.schema<number & tags.Type<"uint32"> & tags.Minimum<10>, "3.0">(),
+  typia.llm.schema<
+    number & tags.Type<"uint32"> & tags.ExclusiveMinimum<10>,
+    "3.0"
+  >(),
 );
