500786d13d6c74e0b35169115cb8956449eb63f1
Jeongho Nam <samchon.github@gmail.com>
2025-04-10 13:37:46 +0900

    Close #1568: issue number is not 1588 but 1568

--- test/src/features/issues/test_issue_1568_native_class_from_node.ts
+++ test/src/features/issues/test_issue_1568_native_class_from_node.ts
@@ -3,7 +3,7 @@ import typia from "typia";
 
 import { TestValidator } from "../../helpers/TestValidator";
 
-export const test_issue_1588_native_class_from_node = (): void => {
+export const test_issue_1568_native_class_from_node = (): void => {
   TestValidator.equals("native")(
     typia.reflect
       .metadata<[Blob | File]>()
