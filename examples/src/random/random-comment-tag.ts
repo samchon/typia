3476006486df386b0103635f23bacbdcbac629cd
Jeongho Nam <samchon.github@gmail.com>
2025-06-22 23:00:04 +0900

    Fix website RemoteSource URL bug (#1595)

---
+++ examples/src/random/random-comment-tag.ts
@@ -0,0 +1,32 @@
+import typia from "typia";
+
+const data: TypeTag = typia.random<TypeTag>();
+console.log(data);
+
+interface TypeTag {
+  /**
+   * @type int
+   */
+  type: number;
+
+  /**
+   * @exclusiveMinimum 19
+   * @maximum 100
+   */
+  number?: number;
+
+  /**
+   * @minLength 3
+   */
+  string: string;
+
+  /**
+   * @pattern ^[a-z]+$
+   */
+  pattern: string;
+
+  /**
+   * @format date-time
+   */
+  format: string | null;
+}
