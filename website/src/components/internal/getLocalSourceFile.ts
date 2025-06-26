5718e2ce9c024181b40726542bd1d5fa11dc6767
Jeongho Nam <samchon.github@gmail.com>
2025-06-27 02:29:51 +0900

    Add new website component `RemoteSource` (#1600)

---
+++ website/src/components/internal/getLocalSourceFile.ts
@@ -0,0 +1,36 @@
+import cp from "child_process";
+import fs from "fs";
+import { Singleton, VariadicSingleton } from "tstl";
+
+export function getLocalSourceFile(location: string): Promise<string> {
+  return loader.get(location);
+}
+
+const loader = new VariadicSingleton(async (location: string) => {
+  await examples.get();
+  const absolute: string = `${await root.get()}/${location}`;
+  const content: string = await fs.promises.readFile(absolute, "utf8");
+  return location.endsWith(".d.ts")
+    ? content.split("    ").join("  ")
+    : content;
+});
+
+const root = new Singleton(async () => {
+  let cwd: string = `${__dirname}/..`;
+  while (true) {
+    cwd += "/..";
+    if (fs.existsSync(`${cwd}/package.json`) === false) continue;
+    const { name } = JSON.parse(
+      await fs.promises.readFile(`${cwd}/package.json`, "utf8"),
+    );
+    if (name === "typia") break;
+  }
+  return cwd;
+});
+
+const examples = new Singleton(async () => {
+  cp.execSync("npm run build", {
+    stdio: "ignore",
+    cwd: `${await root.get()}/examples`,
+  });
+});
