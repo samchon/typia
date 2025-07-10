80f7b657d44aa684dd0b8f987941ccd272c24089
Jeongho Nam <samchon.github@gmail.com>
2025-07-10 12:28:57 +0900

    Change JSON schema title rule.
    ```typescript
    export interface Something {
      /**
       * The first line is not title.
       *
       * The title field can be filled only by `@title` tag.
       */
      first: string;
    
      /**
       * The title value only comes from JSDocTag like below.
       *
       * @title this is the title
       */
      second: number;
    }
    ```

--- src/programmers/internal/json_schema_title.ts
+++ src/programmers/internal/json_schema_title.ts
@@ -9,12 +9,5 @@ export const json_schema_title = (schema: {
   const info: IJsDocTagInfo | undefined = schema.jsDocTags?.find(
     (tag) => tag.name === "title",
   );
-  if (info?.text?.length) return CommentFactory.merge(info.text);
-  else if (!schema.description?.length) return undefined;
-
-  const index: number = schema.description.indexOf("\n");
-  const top: string = (
-    index === -1 ? schema.description : schema.description.substring(0, index)
-  ).trim();
-  return top.endsWith(".") ? top.substring(0, top.length - 1) : undefined;
+  return !!info?.text?.length ? CommentFactory.merge(info.text) : undefined;
 };
