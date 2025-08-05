68da2f3a10e8fdc7c5e670ded1d7367f87cd0da5
Jeongho Nam <samchon.github@gmail.com>
2025-08-06 02:17:13 +0900

    Complete #1631: `typia.llm.application` supports custom validate hook function.

--- src/internal/_llmApplicationFinalize.ts
+++ src/internal/_llmApplicationFinalize.ts
@@ -1,10 +1,15 @@
-import { ILlmApplication, ILlmFunction, ILlmSchema } from "@samchon/openapi";
+import {
+  ILlmApplication,
+  ILlmFunction,
+  ILlmSchema,
+  IValidation,
+} from "@samchon/openapi";
 import { LlmSchemaComposer } from "@samchon/openapi/lib/composers/LlmSchemaComposer";
 
 export const _llmApplicationFinalize = <Model extends ILlmSchema.Model>(
   app: ILlmApplication<Model>,
   options?: Partial<
-    Pick<ILlmApplication.IOptions<Model>, "separate"> & {
+    Pick<ILlmApplication.IOptions<Model>, "separate" | "validate"> & {
       equals?: boolean;
     }
   >,
@@ -12,14 +17,23 @@ export const _llmApplicationFinalize = <Model extends ILlmSchema.Model>(
   app.options = {
     ...LlmSchemaComposer.defaultConfig(app.model),
     separate: options?.separate ?? null,
+    validate: options?.validate ?? null,
   };
-  if (app.options.separate === null) return;
-  for (const func of app.functions)
-    func.separated = LlmSchemaComposer.separateParameters(app.model)({
-      parameters:
-        func.parameters satisfies ILlmSchema.IParameters<Model> as any,
-      predicate: app.options
-        .separate as ILlmApplication.IOptions<Model>["separate"] as any,
-      equals: options?.equals ?? false,
-    }) as ILlmFunction.ISeparated<Model>;
+  if (app.options.separate !== null)
+    for (const func of app.functions)
+      func.separated = LlmSchemaComposer.separateParameters(app.model)({
+        parameters:
+          func.parameters satisfies ILlmSchema.IParameters<Model> as any,
+        predicate: app.options
+          .separate as ILlmApplication.IOptions<Model>["separate"] as any,
+        equals: options?.equals ?? false,
+      }) as ILlmFunction.ISeparated<Model>;
+  if (app.options.validate !== null)
+    for (const func of app.functions)
+      if (typeof app.options.validate?.[func.name] === "function")
+        func.validate = app.options.validate[
+          func.name
+        ]! satisfies Validator as Validator;
 };
+
+type Validator = (input: unknown) => IValidation<unknown>;
