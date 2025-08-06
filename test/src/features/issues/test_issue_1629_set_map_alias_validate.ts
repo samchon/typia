fd9c5d0d5924032217e103f8a2b8b2a7fe9bccfb
Jeongho Nam <samchon.github@gmail.com>
2025-08-06 19:15:47 +0900

    Add test case

---
+++ test/src/features/issues/test_issue_1629_set_map_alias_validate.ts
@@ -0,0 +1,15 @@
+import typia from "typia";
+
+export const test_issue_1629_set_map_alias_validate = (): void => {
+  const m: M = new Map();
+  m.set("first", 1);
+
+  const s: S = new Set();
+  s.set("someothing");
+
+  typia.assert(m);
+  typia.assert(s);
+};
+
+type M = Map<string, number>;
+type S = Set<string>;
