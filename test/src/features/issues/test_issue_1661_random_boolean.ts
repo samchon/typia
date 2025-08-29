f4aab576e5c733bd9812ee30e82cda3b924d5476
Jeongho Nam <samchon.github@gmail.com>
2025-08-29 15:28:30 +0900

    Fix test function

--- test/src/features/issues/test_issue_1661_random_boolean.ts
+++ test/src/features/issues/test_issue_1661_random_boolean.ts
@@ -4,5 +4,7 @@ import { TestValidator } from "../../helpers/TestValidator";
 
 export const test_issue_1661_random_boolean = (): void => {
   const fn: string = typia.createRandom<boolean>().toString();
-  TestValidator.predicate("no argument")(() => fn.includes("_randomBoolean()"));
+  TestValidator.predicate("no argument")(() =>
+    fn.includes("_randomBoolean)()"),
+  );
 };
