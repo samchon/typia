b45a423cb702e097cd413ea3eb7e6582561797ef
Jeongho Nam <samchon.github@gmail.com>
2026-01-22 18:39:04 +0900

    `internals` and `tests` groupping directories

---
+++ internals/benchmark/src/structures/class-validator/ClassValidatorObjectHierarchical.ts
@@ -0,0 +1,136 @@
+import * as tr from "class-transformer";
+import * as cv from "class-validator";
+import "reflect-metadata";
+
+import { ObjectHierarchical } from "../pure/ObjectHierarchical";
+import { ClassValidatorTimestamp } from "./ClassValidatorTimestamp";
+
+class Channel {
+  @cv.IsNumber()
+  public id!: number;
+
+  @cv.IsString()
+  public code!: string;
+
+  @cv.IsString()
+  public name!: string;
+
+  @cv.IsNumber()
+  public sequence!: number;
+
+  @cv.IsBoolean()
+  public exclusive!: boolean;
+
+  @cv.IsNumber()
+  public priority!: number;
+
+  @cv.ValidateNested()
+  @cv.IsObject()
+  @tr.Type(() => ClassValidatorTimestamp)
+  public created_at!: ClassValidatorTimestamp;
+}
+
+class Account {
+  @cv.IsNumber()
+  public id!: number;
+
+  @cv.IsString()
+  public code!: string;
+
+  @cv.ValidateNested()
+  @cv.IsObject()
+  @tr.Type(() => ClassValidatorTimestamp)
+  public created_at!: ClassValidatorTimestamp;
+}
+
+class Member {
+  @cv.IsNumber()
+  public id!: number;
+
+  @cv.ValidateNested()
+  @cv.IsObject()
+  @tr.Type(() => Account)
+  public account!: Account;
+
+  @cv.IsOptional()
+  @cv.ValidateNested()
+  @cv.IsObject()
+  @tr.Type(() => Enterprise)
+  public enterprise!: Enterprise | null;
+
+  @cv.IsArray()
+  @cv.IsString({ each: true })
+  public emails!: string[];
+
+  @cv.ValidateNested()
+  @cv.IsObject()
+  @tr.Type(() => ClassValidatorTimestamp)
+  public created_at!: ClassValidatorTimestamp;
+
+  @cv.IsBoolean()
+  public authorized!: boolean;
+}
+
+class Enterprise {
+  @cv.IsNumber()
+  public id!: number;
+
+  @cv.ValidateNested()
+  @cv.IsObject()
+  @tr.Type(() => Account)
+  public account!: Account;
+
+  @cv.IsString()
+  public name!: string;
+
+  @cv.IsNumber()
+  public grade!: number;
+
+  @cv.ValidateNested()
+  @cv.IsObject()
+  @tr.Type(() => ClassValidatorTimestamp)
+  public created_at!: ClassValidatorTimestamp;
+}
+
+export class ClassValidatorObjectHierarchical {
+  @cv.IsNumber()
+  public id!: number;
+
+  @cv.ValidateNested()
+  @cv.IsObject()
+  @tr.Type(() => Channel)
+  public channel!: Channel;
+
+  @cv.IsOptional()
+  @cv.ValidateNested()
+  @cv.IsObject()
+  @tr.Type(() => Member)
+  public member!: Member | null;
+
+  @cv.IsOptional()
+  @cv.ValidateNested()
+  @cv.IsObject()
+  @tr.Type(() => Account)
+  public account!: Account | null;
+
+  @cv.IsString()
+  public href!: string;
+
+  @cv.IsString()
+  public referrer!: string;
+
+  @cv.IsArray()
+  @cv.IsNumber({}, { each: true })
+  public ip!: [number, number, number, number];
+
+  @cv.ValidateNested()
+  @cv.IsObject()
+  @tr.Type(() => ClassValidatorTimestamp)
+  public created_at!: ClassValidatorTimestamp;
+}
+export namespace ClassValidatorObjectHierarchical {
+  export const transform = (input: ObjectHierarchical) =>
+    tr.plainToInstance(ClassValidatorObjectHierarchical, input);
+  export const validate = (input: ObjectHierarchical) =>
+    cv.validateSync(transform(input));
+}
---
+++ internals/benchmark/src/structures/class-validator/ClassValidatorObjectUnionImplicit.ts
@@ -0,0 +1,115 @@
+import * as tr from "class-transformer";
+import * as cv from "class-validator";
+import "reflect-metadata";
+
+import { ObjectUnionImplicit } from "../pure/ObjectUnionImplicit";
+
+class Point {
+  @cv.IsNumber()
+  x!: number;
+
+  @cv.IsNumber()
+  y!: number;
+}
+
+class Polyline {
+  @tr.Type(() => Point)
+  @cv.IsArray()
+  @cv.IsObject({ each: true })
+  @cv.ValidateNested({ each: true })
+  points!: Point[];
+}
+
+export class ClassValidatorObjectUnionImplicit {
+  @cv.IsOptional()
+  @cv.IsNumber()
+  x?: number;
+
+  @cv.IsOptional()
+  @cv.IsNumber()
+  y?: number;
+
+  @cv.IsOptional()
+  @cv.IsNumber()
+  slope?: number;
+
+  @cv.IsOptional()
+  @cv.ValidateNested()
+  @cv.IsObject()
+  @tr.Type(() => Point)
+  p1?: Point;
+
+  @cv.IsOptional()
+  @cv.ValidateNested()
+  @cv.IsObject()
+  @tr.Type(() => Point)
+  p2?: Point;
+
+  @cv.IsOptional()
+  @cv.ValidateNested()
+  @cv.IsObject()
+  @tr.Type(() => Point)
+  p3?: Point;
+
+  @cv.IsOptional()
+  @cv.ValidateNested()
+  @cv.IsObject()
+  @tr.Type(() => Point)
+  p4?: Point;
+
+  @tr.Type(() => Point)
+  @cv.IsArray()
+  @cv.IsObject({ each: true })
+  @cv.ValidateNested({ each: true })
+  @cv.IsOptional()
+  points?: Point[];
+
+  @cv.IsOptional()
+  @cv.ValidateNested()
+  @cv.IsObject()
+  @tr.Type(() => Polyline)
+  outer?: Polyline;
+
+  @tr.Type(() => Polyline)
+  @cv.IsArray()
+  @cv.IsObject({ each: true })
+  @cv.ValidateNested({ each: true })
+  @cv.IsOptional()
+  inner?: Polyline[];
+
+  @cv.IsOptional()
+  @cv.ValidateNested()
+  @cv.IsObject()
+  @tr.Type(() => Point)
+  centroid?: Point;
+
+  @cv.IsOptional()
+  @cv.IsNumber()
+  radius?: number;
+
+  @cv.IsOptional()
+  @cv.IsNumber()
+  distance?: number;
+
+  @cv.IsOptional()
+  @cv.IsNumber()
+  width?: number;
+
+  @cv.IsOptional()
+  @cv.IsNumber()
+  height?: number;
+
+  @cv.IsOptional()
+  @cv.IsNumber()
+  area?: number;
+
+  @cv.IsOptional()
+  @cv.IsNumber()
+  length?: number;
+}
+export namespace ClassValidatorObjectUnionImplicit {
+  export const transform = (input: ObjectUnionImplicit) =>
+    tr.plainToInstance(ClassValidatorObjectUnionImplicit, input);
+  export const validate = (input: ObjectUnionImplicit) =>
+    cv.validateSync(transform(input));
+}
