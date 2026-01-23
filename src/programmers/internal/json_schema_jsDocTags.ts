9db2aad40d2880e5960e6e2eccbd8cb85065d698
Jeongho Nam <samchon.github@gmail.com>
2026-01-23 17:55:06 +0900

    JSON schema supports `@x-plugin null` comment. (#1717)
    * JSON schema supports `@x-plugin null` comment.
    
    * trim

--- src/programmers/internal/json_schema_jsDocTags.ts
+++ src/programmers/internal/json_schema_jsDocTags.ts
@@ -11,17 +11,17 @@ export const json_schema_jsDocTags = <Schema extends OpenApi.IJsonSchema>(
 
     const value: string | undefined = tag.text
       ?.filter((s) => s.kind === "text")
-      .map((s) => s.text.split("\r\n").join("\n"))[0];
+      .map((s) => s.text.trim().split("\r\n").join("\n"))[0];
     if (value === undefined) continue;
     else (schema as any)[tag.name] = cast(value);
   }
   return schema;
 };
 
-const cast = (value: string): string | number | boolean => {
+const cast = (value: string): string | number | boolean | null => {
   if (value === "true") return true;
   if (value === "false") return false;
   const num: number = Number(value);
   if (Number.isNaN(num) === false) return num;
-  return value;
+  return value === "null" ? null : value;
 };
