ffca011c93905e13860cfc73edb5f9e169353174
Jeongho Nam <samchon.github@gmail.com>
2026-03-28 02:17:15 +0900

    erase custom error pages

--- website/src/pages/500.tsx
+++
@@ -1,8 +0,0 @@
-export default function Custom500Page() {
-  return (
-    <main style={{ padding: "3rem 1.5rem", fontFamily: "sans-serif" }}>
-      <h1>500</h1>
-      <p>Something went wrong while rendering this page.</p>
-    </main>
-  );
-}
