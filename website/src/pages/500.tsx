0ff377d6501cbaa1dd3e69c2318594692f1fbfcf
Jeongho Nam <samchon.github@gmail.com>
2026-03-28 02:13:25 +0900

    fix website 500 error?

---
+++ website/src/pages/500.tsx
@@ -0,0 +1,8 @@
+export default function Custom500Page() {
+  return (
+    <main style={{ padding: "3rem 1.5rem", fontFamily: "sans-serif" }}>
+      <h1>500</h1>
+      <p>Something went wrong while rendering this page.</p>
+    </main>
+  );
+}
