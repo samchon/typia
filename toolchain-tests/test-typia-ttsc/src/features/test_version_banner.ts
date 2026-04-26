b1f0c662414007334077b18776a676a0176c0c79
Jeongho Nam <samchon.github@gmail.com>
2026-04-26 11:57:44 +0900

    feat(config): no more `downlevelIteration` provided. (#1823)
    * feat(config): no more `downlevelIteration` provided.
    
    * fix ttsc version in experiments
    
    * bump up typescript version
    
    * bump up typescript version
    
    * bump up to dieted ttsc

--- toolchain-tests/test-typia-ttsc/src/features/test_version_banner.ts
+++ toolchain-tests/test-typia-ttsc/src/features/test_version_banner.ts
@@ -5,7 +5,7 @@ import { runTtsc } from "../utils/runTtsc";
 export async function test_version_banner(): Promise<void> {
   const { stdout, status } = runTtsc(["--version"]);
   assert.equal(status, 0, "ttsc --version must exit 0");
-  for (const fragment of ["ttsc ", "commit", "go"]) {
+  for (const fragment of ["ttsc ", "(Version ", ")"]) {
     assert.ok(
       stdout.includes(fragment),
       `version banner should include ${JSON.stringify(fragment)}: ${stdout}`,
