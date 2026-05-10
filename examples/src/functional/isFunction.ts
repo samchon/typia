cf59833109ae60c03e534c823d7161814c6dbf06
Jeongho Nam <samchon.github@gmail.com>
2026-05-11 03:15:44 +0900

    fix format

--- examples/src/functional/isFunction.ts
+++ examples/src/functional/isFunction.ts
@@ -1,8 +1,6 @@
 import typia from "typia";
 
-const func = typia.functional.isFunction<
-  (x: number, y: number) => number
->(
+const func = typia.functional.isFunction<(x: number, y: number) => number>(
   (x: number, y: number): number => x + y,
 );
 func(3, 4);
