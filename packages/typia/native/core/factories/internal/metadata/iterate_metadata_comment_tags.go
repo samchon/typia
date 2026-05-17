c42f1079fc6392b78d86496e26f3cd49cf686f41
samchon <samchon.github@gmail.com>
2026-05-18 02:34:38 +0900

    fix format

--- packages/typia/native/core/factories/internal/metadata/iterate_metadata_comment_tags.go
+++ packages/typia/native/core/factories/internal/metadata/iterate_metadata_comment_tags.go
@@ -3,32 +3,32 @@ package metadata
 import schemametadata "github.com/samchon/typia/packages/typia/native/core/schemas/metadata"
 
 func Iterate_metadata_comment_tags(props struct {
-	Errors *[]MetadataFactory_IError
-	Object *schemametadata.MetadataObjectType
+  Errors *[]MetadataFactory_IError
+  Object *schemametadata.MetadataObjectType
 }) {
-	if props.Object == nil || props.Object.Tagged_ == true || MetadataCommentTagAnalyzer == nil {
-		return
-	}
-	props.Object.Tagged_ = true
+  if props.Object == nil || props.Object.Tagged_ == true || MetadataCommentTagAnalyzer == nil {
+    return
+  }
+  props.Object.Tagged_ = true
 
-	for _, property := range props.Object.Properties {
-		if property == nil || property.Value == nil || len(property.JsDocTags) == 0 {
-			continue
-		}
-		MetadataCommentTagAnalyzer(struct {
-			Errors   *[]MetadataFactory_IError
-			Metadata *schemametadata.MetadataSchema
-			Tags     []schemametadata.IJsDocTagInfo
-			Explore  MetadataFactory_IExplore
-		}{
-			Errors:   props.Errors,
-			Metadata: property.Value,
-			Tags:     property.JsDocTags,
-			Explore: MetadataFactory_IExplore{
-				Top:      false,
-				Object:   props.Object,
-				Property: property,
-			},
-		})
-	}
+  for _, property := range props.Object.Properties {
+    if property == nil || property.Value == nil || len(property.JsDocTags) == 0 {
+      continue
+    }
+    MetadataCommentTagAnalyzer(struct {
+      Errors   *[]MetadataFactory_IError
+      Metadata *schemametadata.MetadataSchema
+      Tags     []schemametadata.IJsDocTagInfo
+      Explore  MetadataFactory_IExplore
+    }{
+      Errors:   props.Errors,
+      Metadata: property.Value,
+      Tags:     property.JsDocTags,
+      Explore: MetadataFactory_IExplore{
+        Top:      false,
+        Object:   props.Object,
+        Property: property,
+      },
+    })
+  }
 }
