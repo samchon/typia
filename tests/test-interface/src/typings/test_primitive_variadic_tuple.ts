15b249178e2748a49ed2077fd13dbef3c3fa5d53
Jeongho Nam <samchon.github@gmail.com>
2026-06-08 16:47:04 +0900

    test(typia): tighten primitive tuple type coverage

--- tests/test-interface/src/typings/test_primitive_variadic_tuple.ts
+++ tests/test-interface/src/typings/test_primitive_variadic_tuple.ts
@@ -1,25 +1,25 @@
 import { Primitive, tags } from "@typia/interface";
 
 export type PrimitiveVariadicTupleCases = [
-  AssertAssignable<ExpectedLiteralVariadic, LiteralVariadic>,
-  AssertAssignable<LiteralVariadic, ExpectedLiteralVariadic>,
-  AssertAssignable<ExpectedDateRest, DateRest>,
-  AssertAssignable<DateRest, ExpectedDateRest>,
-  AssertAssignable<ExpectedObjectRest, ObjectRest>,
-  AssertAssignable<ObjectRest, ExpectedObjectRest>,
-  AssertAssignable<ExpectedOptionalTuple, OptionalTuple>,
-  AssertAssignable<OptionalTuple, ExpectedOptionalTuple>,
-  AssertAssignable<ExpectedStringArray, StringArray>,
-  AssertAssignable<StringArray, ExpectedStringArray>,
-  AssertAssignable<ExpectedDateArray, DateArray>,
-  AssertAssignable<DateArray, ExpectedDateArray>,
-  AssertAssignable<ExpectedObjectArray, ObjectArray>,
-  AssertAssignable<ObjectArray, ExpectedObjectArray>,
-  AssertAssignable<ExpectedUnionTuple, UnionTuple>,
-  AssertAssignable<UnionTuple, ExpectedUnionTuple>,
+  Assert<IsEqual<LiteralVariadic, ExpectedLiteralVariadic>>,
+  Assert<IsEqual<DateRest, ExpectedDateRest>>,
+  Assert<IsEqual<ObjectRest, ExpectedObjectRest>>,
+  Assert<IsEqual<OptionalTuple, ExpectedOptionalTuple>>,
+  Assert<IsEqual<StringArray, ExpectedStringArray>>,
+  Assert<IsEqual<DateArray, ExpectedDateArray>>,
+  Assert<IsEqual<ObjectArray, ExpectedObjectArray>>,
+  Assert<IsEqual<UnionTuple, ExpectedUnionTuple>>,
 ];
 
+export const validLiteralWithoutMiddle: LiteralVariadic = ["asdf", "zxcv"];
 export const validLiteral: LiteralVariadic = ["asdf", "middle", "zxcv"];
+export const validLiteralWithLongMiddle: LiteralVariadic = [
+  "asdf",
+  "a",
+  "b",
+  "c",
+  "zxcv",
+];
 
 // @ts-expect-error literal head must not widen to string.
 export const invalidHead: LiteralVariadic = ["qwer", "middle", "zxcv"];
@@ -27,9 +27,43 @@ export const invalidHead: LiteralVariadic = ["qwer", "middle", "zxcv"];
 // @ts-expect-error literal tail must not widen to string.
 export const invalidTail: LiteralVariadic = ["asdf", "middle", "qwer"];
 
-type AssertAssignable<Expected, Actual> = [Actual] extends [Expected]
-  ? true
-  : never;
+export const validDateRest: DateRest = [
+  "head",
+  "2026-06-08T00:00:00.000Z" as DateTime,
+  "tail",
+];
+
+// @ts-expect-error Date rest elements must be converted to date-time strings.
+export const invalidDateRest: DateRest = ["head", new Date(), "tail"];
+
+export const validObjectRest: ObjectRest = [
+  "2026-06-08T00:00:00.000Z" as DateTime,
+  { value: 1 },
+  { done: true },
+];
+
+// @ts-expect-error boxed object rest property must be converted to number.
+export const invalidObjectRestValue: ObjectRest = [
+  "2026-06-08T00:00:00.000Z" as DateTime,
+  { value: new Number(1) },
+  { done: true },
+];
+
+// @ts-expect-error boxed tail property must be converted to boolean.
+export const invalidObjectRestTail: ObjectRest = [
+  "2026-06-08T00:00:00.000Z" as DateTime,
+  { value: 1 },
+  { done: new Boolean(true) },
+];
+
+type Assert<T extends true> = T;
+
+type IsEqual<X, Y> =
+  (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y ? 1 : 2
+    ? (<T>() => T extends Y ? 1 : 2) extends <T>() => T extends X ? 1 : 2
+      ? true
+      : false
+    : false;
 
 type DateTime = string & tags.Format<"date-time">;
 
