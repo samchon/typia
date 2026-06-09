19e40423b7fb2dfb8abe08057012146addbc796d
Jeongho Nam <samchon.github@gmail.com>
2026-06-10 08:00:31 +0900

    test(typia): align transform runtime stubs

--- packages/typia/native/cmd/ttsc-typia/transform_test_helpers.go
+++ packages/typia/native/cmd/ttsc-typia/transform_test_helpers.go
@@ -95,8 +95,47 @@ const ttscTypiaTestAssertGuardStub = `module.exports._assertGuard = (exceptionab
 };
 `
 
-const ttscTypiaTestAccessExpressionStub = `module.exports._accessExpressionAsString = (key) => {
-  return /^[A-Za-z_$][0-9A-Za-z_$]*$/.test(key)
+const ttscTypiaTestAccessExpressionStub = `const reserved = new Set([
+  "break",
+  "case",
+  "catch",
+  "class",
+  "const",
+  "continue",
+  "debugger",
+  "default",
+  "delete",
+  "do",
+  "else",
+  "enum",
+  "export",
+  "extends",
+  "false",
+  "finally",
+  "for",
+  "function",
+  "if",
+  "import",
+  "in",
+  "instanceof",
+  "new",
+  "null",
+  "return",
+  "super",
+  "switch",
+  "this",
+  "throw",
+  "true",
+  "try",
+  "typeof",
+  "var",
+  "void",
+  "while",
+  "with",
+]);
+
+module.exports._accessExpressionAsString = (key) => {
+  return reserved.has(key) === false && /^[A-Za-z_$][0-9A-Za-z_$]*$/.test(key)
     ? "." + key
     : "[" + JSON.stringify(key) + "]";
 };
