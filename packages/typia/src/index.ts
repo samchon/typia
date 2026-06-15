845f9f90c0a632db126653e1ebe667091a3ae983
Jeongho Nam <samchon.github@gmail.com>
2026-06-16 02:49:26 +0900

    feat(typia): export `createTtscPlugin` from index. (#1946)

--- packages/typia/src/index.ts
+++ packages/typia/src/index.ts
@@ -1,4 +1,9 @@
 import * as typia from "./module";
+/** @internal */
+import createTtscPlugin from "./transform";
 
 export default typia;
 export * from "./module";
+
+/** @internal */
+export { createTtscPlugin };
